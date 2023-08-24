e05bf01c4d8f4af16fb3179a4005da004a5e3238
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-24 07:58:42 +0600

    Selection Sorting

---
+++ src/SortingProblems/SelectionSortEx1.java
@@ -0,0 +1,45 @@
+package SortingProblems;
+
+public class SelectionSortEx1 {
+    public void selectionSort(int[] array) {
+        int number = array.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < number - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < number; j++)
+                if (array[j] < array[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first element.
+            int temporaryValue = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temporaryValue;
+        }
+    }
+
+    // Prints the array
+    void printArray(int[] array) {
+//        int number = array.length;
+        for (int j : array) System.out.print(j + " ");
+        System.out.println();
+    }
+
+    // Driver code to test above
+    public static void main(String[] args) {
+        SelectionSortEx1 selectionSortEx1 = new SelectionSortEx1();
+        int[] array = {64, 25, 12, 22, 11};
+        selectionSortEx1.selectionSort(array);
+        System.out.println("Sorted Array");
+        selectionSortEx1.printArray(array);
+    }
+}
+
+/*
+Expected Output::
+
+Sorted Array
+11 12 22 25 64
+
+* */
---
+++ src/SortingProblems/SelectionSortingEx2.java
@@ -0,0 +1,35 @@
+package SortingProblems;
+
+public class SelectionSortingEx2 {
+    public static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {7, 8, 3, 1, 2};
+
+        // Selection Sorting
+        for (int i = 0; i < array.length; i++) {
+            int smallestIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[smallestIndex] > array[j]) {
+                    smallestIndex = j;
+                }
+            }
+            int temporaryValue = array[smallestIndex];
+            array[smallestIndex] = array[i];
+            array[i] = temporaryValue;
+        }
+        System.out.println("Sorted Arrays");
+        printArray(array);
+    }
+}
+
+/*
+Expected Output::
+
+Sorted Arrays
+1 2 3 7 8
+* */
