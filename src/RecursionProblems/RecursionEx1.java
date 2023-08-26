a76c47c2eb63b3a21b82457ef23259ea636b64ac
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-27 02:39:05 +0600

    Recursion

--- src/RecursionProblems/RecursionEx1.java
+++ src/RecursionProblems/RecursionEx1.java
@@ -1,28 +1,38 @@
 package RecursionProblems;
 
 public class RecursionEx1 {
-    public static void printNumber(int number) {
-        if (number == 0) {  // Base Case
+    public static void printNumber5_1(int number1) {
+        if (number1 == 0) {  // Base Case
             return;
         }
-        System.out.println(number);
+        System.out.print(number1 + " ");
 
-        printNumber(number - 1); // Recursion
+        printNumber5_1(number1 - 1); // Recursion
+    }
+
+    public static void printNumber1_5(int number2) {
+        if (number2 == 6) {  // Base Case
+            return;
+        }
+        System.out.print(number2 + " ");
+
+        printNumber1_5(number2 + 1); // Recursion
     }
 
     public static void main(String[] args) {
-        int number = 5;
-        printNumber(number);
+        int number1 = 5;
+        int number2 = 1;
+        printNumber5_1(number1);  // Print Number 5 to 1
+        System.out.println("\n");
+        printNumber1_5(number2);  // // Print Number 1 to 5
     }
 }
 
 /*
 Expected Output::
 
-5
-4
-3
-2
-1
+5 4 3 2 1
+
+1 2 3 4 5
 
 * */
