efdb8a2ec0ecaa800dcd824d8dbb47c857d4e97f
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-27 15:17:15 +0600

    Recursion

---
+++ src/RecursionProblems/PrintStringReverse.java
@@ -0,0 +1,24 @@
+package RecursionProblems;
+
+public class PrintStringReverse {
+    public static void printReverse(String string, int index) {
+        if (index == 0) {
+            System.out.print(string.charAt(index) + " ");
+            return;
+        }
+        System.out.print(string.charAt(index) + " ");
+        printReverse(string, index - 1);
+    }
+
+    public static void main(String[] args) {
+        String string = "abcd";
+        printReverse(string, string.length() - 1);
+    }
+}
+
+/*
+Expected Output::
+
+d c b a
+
+* */
