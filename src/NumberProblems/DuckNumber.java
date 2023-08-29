cce92096b0a32cd3134fa00f6c2d6abeb6f10b96
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-29 21:56:47 +0600

    DuckNumber

---
+++ src/NumberProblems/DuckNumber.java
@@ -0,0 +1,39 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+/*Duck Number Program in Java
+A Duck number is a number which has zeroes present in it,
+but there should be no zero present in the beginning of the number.
+For example 3210*/
+
+public class DuckNumber {
+    public static void main(String[] args) {
+        int r, n, num;
+        boolean flag = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number=");
+        n = sc.nextInt();
+        num = n;
+        while (num > 0) {
+            r = num % 10;
+            if (r == 0) {
+                flag = true;
+            }
+            num = num / 10;
+        }
+        if (flag) {
+            System.out.println("Duck Number");
+        } else {
+            System.out.println("Not Duck Number");
+        }
+    }
+}
+
+/*
+Expected Output::
+
+Enter number=55
+Not Duck Number
+
+* */
