5096eafc2b08b37b800814a5a1995dba8a167270
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-31 03:06:39 +0600

    MultiplyOfDigit

---
+++ src/NumberProblems/MultiplyOfDigit.java
@@ -0,0 +1,32 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+/*Multiply Of Digit Program in Java
+If a number=1234, then 1*2*3*4 ,Multiply of digit=24 */
+
+public class MultiplyOfDigit {
+    public static void main(String[] args) {
+        int r, n, num, mul = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number=");
+        n = sc.nextInt();
+        num = n;
+        while (num > 0) {
+            r = num % 10;
+            mul = mul * r;
+            num = num / 10;
+        }
+        System.out.println("Multiply of digit=" + mul);
+    }
+}
+
+/*
+Expected Output::
+
+Enter number=44
+Multiply of digit=16
+
+
+
+* */
