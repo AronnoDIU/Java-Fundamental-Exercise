e0811b3fb0dc7b896b28b4ae4269d3cd6c2e879b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-31 11:45:41 +0600

    Place Tiles in a Floor

---
+++ src/RecursionProblems/PlaceTilesInAFloor.java
@@ -0,0 +1,39 @@
+package RecursionProblems;
+
+// Place Tiles of size 1*m in a floor of size n*m
+
+public class PlaceTilesInAFloor {
+
+    public static int tilesPlacements(int n, int m) {
+
+        if (n == m) {
+            return 2;
+        }
+
+        if (n < m) {
+            return 1;
+        }
+
+
+        // Vertical Placements
+        int verticalPlacements = tilesPlacements(n - m, m);
+
+        // Horizontal Placements
+        int horizontalPlacements = tilesPlacements(n - 1, m);
+
+        return verticalPlacements + horizontalPlacements;
+    }
+
+    public static void main(String[] args) {
+        int n = 4, m = 2;
+
+        System.out.println("Total ways of Tiles Placements " + tilesPlacements(n, m));
+    }
+}
+
+/*
+Expected Output::
+
+Total ways of Tiles Placements 5
+
+* */
