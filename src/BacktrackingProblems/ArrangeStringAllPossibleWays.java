a1d54286db48cea9955c4cc582cda77c6469a607
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-01 07:00:49 +0600

    ArrangeStringAllPossibleWays

---
+++ src/BacktrackingProblems/ArrangeStringAllPossibleWays.java
@@ -0,0 +1,37 @@
+package BacktrackingProblems;
+
+// Arrange them in a Single line
+public class ArrangeStringAllPossibleWays {
+    public static void
+    printPermutations(String string, String permutations, int index) {
+
+        if (string.isEmpty()) {
+            System.out.println(permutations);
+            return;
+        }
+
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentCharacter = string.charAt(i);
+            String newString = string.substring(0, i) + string.substring(i + 1);
+            printPermutations(newString, permutations + currentCharacter, index + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String string = "ABC";
+        printPermutations(string, "", 0);
+    }
+}
+
+/*
+Expected Output::
+
+ABC
+ACB
+BAC
+BCA
+CAB
+CBA
+
+* */
