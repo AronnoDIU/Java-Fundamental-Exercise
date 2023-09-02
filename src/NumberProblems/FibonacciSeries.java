11edffbe442188367f9ed3fc9b1a11bc6e8687ad
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-02 14:05:50 +0600

    Fibonacci Series

--- src/NumberProblems/FibonacciSeries.java
+++ src/NumberProblems/FibonacciSeries.java
@@ -2,22 +2,22 @@ package NumberProblems;
 
 import java.util.Scanner;
 
-/*A series of numbers in which each number ( Fibonacci number ) is the sum
+/*A series of numbers in which each number (Fibonacci number) is the sum
 of the two preceding numbers. The simplest is the series 0, 1, 1, 2, 3, 5, 8, etc.*/
 
 public class FibonacciSeries {
     public static void main(String[] args) {
         int number;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of series=");
+        System.out.print("Enter number of series = ");
         number = sc.nextInt();
-        int a = 0,
-                b = 1, c;
+        int firstNumber = 0,
+                secondNumber = 1, thirdNumber;
         for (int i = 1; i <= number; i++) {
-            System.out.println(a);
-            c = a + b;
-            a = b;
-            b = c;
+            System.out.println(firstNumber);
+            thirdNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
         }
     }
 }
@@ -25,7 +25,7 @@ public class FibonacciSeries {
 /*
 Expected Output::
 
-Enter number of series=5
+Enter number of series = 5
 0
 1
 1
