d6c10cf6171c0d8df9e67157525e3ca289ea8007
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-08 07:41:23 +0600

    CalendarExample

--- src/Miscellaneous/CalendarExample.java
+++ src/Miscellaneous/CalendarExample.java
@@ -15,6 +15,31 @@ public class CalendarExample {
 //        calendar.set(Calendar.SECOND, 0);
         Date date = calendar.getTime();
         System.out.println(date);
+
+        /* In this example, calendar1 is set to August 1, 2023, and calendar2 is set to
+        September 1, 2023. Then, the .after() method is used to compare these two instances.*/
+
+
+        // Create two instances of Calendar
+        boolean isAfter = isAfter();
+
+        if (isAfter) {
+            System.out.println("calendar1 is after calendar2");
+        } else {
+            System.out.println("calendar1 is not after calendar2");
+        }
+    }
+
+    private static boolean isAfter() {
+        Calendar calendar1 = Calendar.getInstance();
+        Calendar calendar2 = Calendar.getInstance();
+
+        // Set different dates for each instance
+        calendar1.set(2023, Calendar.AUGUST, 1); // August 1, 2023
+        calendar2.set(2023, Calendar.SEPTEMBER, 1); // September 1, 2023
+
+        // Check if calendar1 is after calendar2
+        return calendar1.after(calendar2);
     }
 }
 
