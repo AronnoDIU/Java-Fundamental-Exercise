1c327f64166bfb6472c73658ee86303ae8438d19
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-10 21:01:29 +0600

    generateNBitGrayCodes

--- src/BacktrackingProblems/generateNBitGrayCodes.java
+++ src/BacktrackingProblems/generateNBitGrayCodes.java
@@ -22,40 +22,52 @@ import java.util.Vector;
 public class generateNBitGrayCodes {
     static int num;
 
-    /* we have 2 choices for each of the n bits either we can include i.e.,
+    /* we have 2 choices for each of the number bits either we can include i.e.,
     invert the bit, or we can exclude the bit i.e., we can leave the number as it is. */
-    static void grayCodeUtil(Vector<Integer> res, int n) {
+    static void grayCodeUtil(Vector<Integer> res, int number) {
         // base case when we run our bits to process,
         // we simply include it in a gray code sequence.
-        if (n == 0) {
+        if (number == 0) {
             res.add(num);
             return;
         }
 
         // ignore the bit.
-        grayCodeUtil(res, n - 1);
+        grayCodeUtil(res, number - 1);
 
         // invert the bit.
-        num = num ^ (1 << (n - 1));
-        grayCodeUtil(res, n - 1);
+        num = num ^ (1 << (number - 1));
+        grayCodeUtil(res, number - 1);
     }
 
-    // returns the vector containing the gray
-// code sequence of n bits.
-    static Vector<Integer> grayCodes(int n)
-    {
-        Vector<Integer> res = new Vector<Integer>();
+    // returns the vector containing the gray code sequence of number bits.
+    static Vector<Integer> grayCodes(int number) {
+        Vector<Integer> res = new Vector<>();
 
         // num is passed by reference to keep
         // track of current code.
         num = 0;
-        grayCodeUtil(res, n);
+        grayCodeUtil(res, number);
 
         return res;
     }
+
     public static void main(String[] args) {
-        int n = 3;
-        Vector<Integer> code = grayCodes(n);
+        int number = 3;
+        Vector<Integer> code = grayCodes(number);
         for (Integer integer : code) System.out.print(integer + "\n");
     }
 }
+
+/*Expected Output::
+
+0
+1
+3
+2
+6
+7
+5
+4
+
+* */
