c6a9103669a697460f278ea85e1a72adc13e2ed4
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-16 17:09:02 +0600

    LinkedList

---
+++ src/CollectionsFramework/LinkedListEx1.java
@@ -0,0 +1,40 @@
+package CollectionsFramework;
+
+import java.util.LinkedList;
+
+/*#LinkedList:
+    The LinkedList class is an implementation of the LinkedList data structure which is a linear
+    data structure where the elements are not stored in contiguous locations and every element
+    is a separate object with a data part and address part. The elements are linked using
+    pointers and addresses. Each element is known as a node. */
+public class LinkedListEx1 {
+    public static void main(String[] args) {
+        // Declaring the LinkedList
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+
+        // Appending new elements at the end of the list
+        for (int index = 1; index <= 5; index++)
+            integerLinkedList.add(index);
+
+        // Printing elements
+        System.out.println(integerLinkedList);
+
+        // Remove element at index 3
+        integerLinkedList.remove(3);
+
+        // Displaying the List after deletion
+        System.out.println(integerLinkedList);
+
+        // Printing elements one by one
+        for (Integer EnhancedLinkedList : integerLinkedList)
+            System.out.print(EnhancedLinkedList + " ");
+    }
+}
+
+/*Expected Output:
+
+[1, 2, 3, 4, 5]
+[1, 2, 3, 5]
+1 2 3 5
+
+* */
