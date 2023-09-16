0e446ae2e986f8b0909a1e5608a15951bb84ca81
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-16 18:31:24 +0600

    LinkedHashSet

---
+++ src/CollectionsFramework/LinkedHashSetEx1.java
@@ -0,0 +1,34 @@
+package CollectionsFramework;
+
+import java.util.LinkedHashSet;
+
+/*#LinkedHashSet:
+    A LinkedHashSet is very similar to a HashSet. The difference is that this uses a doubly
+    linked list to store the data and retains the ordering of the elements. */
+public class LinkedHashSetEx1 {
+    public static void main(String[] args) {
+        // Creating LinkedHashSet and adding elements
+        LinkedHashSet<String> stringLinkedHashSet = new LinkedHashSet<>();
+
+        stringLinkedHashSet.add("This");
+        stringLinkedHashSet.add("is");
+        stringLinkedHashSet.add("Yeasir");
+        stringLinkedHashSet.add("Arafat");
+        stringLinkedHashSet.add("Aronno here");
+
+        // Traversing elements
+        for (String EnhancedLinkedHashSet : stringLinkedHashSet) {
+            System.out.println(EnhancedLinkedHashSet);
+        }
+    }
+}
+
+/*Expected Output:
+
+This
+is
+Yeasir
+Arafat
+Aronno here
+
+* */
