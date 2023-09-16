563960ef2879f860b6c8b947640eead8ad9e8308
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-16 18:52:50 +0600

    ArrayDeque

--- src/CollectionsFramework/ArrayDequeEx1.java
+++ src/CollectionsFramework/ArrayDequeEx1.java
@@ -2,14 +2,14 @@ package CollectionsFramework;
 
 import java.util.ArrayDeque;
 
-/*#Deque Interface:
+/*#Deque Interface: (Double Ended Queue)
     This is a very slight variation of the queue data structure. Deque, also known as a double-ended
     queue, is a data structure where we can add and remove elements from both ends of the queue.
     This interface extends the queue interface. The class which implements this interface is
     ArrayDeque. Since ArrayDeque class implements the Deque interface, we can instantiate a
     deque object with this class.
 
-    For example:
+    For example,
     Deque<T> ad = new ArrayDeque<> ();
     Where T is the type of the object.
 
@@ -17,9 +17,9 @@ The class which implements the DEQUE interface is ArrayDeque.
 
 #ArrayDeque:
     ArrayDeque class which is implemented in the collection framework provides us with a way
-    to apply resizable array. This is a special kind of array that grows and allows users
+    to apply a resizable array. This is a special kind of array that grows and allows users
     to add or remove an element from both sides of the queue. Array dequeue have no
-    capacity restrictions and they grow as necessary to support usage. */
+    capacity restrictions, and they grow as necessary to support usage. */
 public class ArrayDequeEx1 {
     public static void main(String[] args) {
         // Initializing an deque
