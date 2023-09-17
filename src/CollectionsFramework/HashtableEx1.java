0f1ffdad658104d886d6ea9fe048bd7555a1755c
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-17 09:55:58 +0600

    Hashtable

---
+++ src/CollectionsFramework/HashtableEx1.java
@@ -0,0 +1,51 @@
+package CollectionsFramework;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+/*The Hashtable class: implements a hash table, which maps keys to values. Any non-null object can be
+used as a key or as a value. To successfully store and retrieve objects from a hashtable, the
+objects used as keys must implement the hashCode method and the equals method. The java.util.Hashtable
+class is a class in Java that provides a key-value data structure, similar to the Map interface.
+
+However, the Hashtable class has since been considered obsolete and its use is generally discouraged.
+This is because it was designed prior to the introduction of the Collections framework and does not
+implement the Map interface, which makes it difficult to use in conjunction with other parts of the framework. In addition, the Hashtable class is synchronized, which can result in slower performance compared to other implementations of the Map interface.
+
+In general, it’s recommended to use the Map interface or one of its implementations (such as HashMap
+or ConcurrentHashMap) instead of the Hashtable class.*/
+public class HashtableEx1 {
+    public static void main(String[] args) {
+        Hashtable<String, Integer> stringIntegerHashtable = new Hashtable<>();
+
+        // Adding elements to the stringIntegerHashtable
+        stringIntegerHashtable.put("A", 1);
+        stringIntegerHashtable.put("B", 2);
+        stringIntegerHashtable.put("C", 3);
+        stringIntegerHashtable.put("D", 4);
+
+        // Getting values from the stringIntegerHashtable
+        int gettingValues = stringIntegerHashtable.get("A");
+        System.out.println("Value of A: " + gettingValues);
+
+        // Removing elements from the stringIntegerHashtable
+        stringIntegerHashtable.remove("B");
+
+        // Enumerating the elements of the stringIntegerHashtable
+        Enumeration<String> keys = stringIntegerHashtable.keys();
+
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            System.out.println("Key: " + key + ", Value: " + stringIntegerHashtable.get(key));
+        }
+    }
+}
+
+/*Expected Output:
+
+Value of A: 1
+Key: A, Value: 1
+Key: D, Value: 4
+Key: C, Value: 3
+
+* */
