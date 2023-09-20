a744775c59a9e1e228d35d7a8ea392727ed46287
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-20 22:20:46 +0600

    HashMap

--- src/CollectionsFramework/HashMapEx2.java
+++ src/CollectionsFramework/HashMapEx2.java
@@ -13,11 +13,48 @@ public class HashMapEx2 {
         capitalCities.put("Norway", "Oslo");
         capitalCities.put("USA", "Washington DC");
         System.out.println(capitalCities);
+
+        // Access an Item
+        capitalCities.get("England");
+
+        // Remove an Item
+        capitalCities.remove("England");
+
+        // HashMap Size
+//        capitalCities.size();
+
+        // Loop Through a HashMap
+        // Print keys
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        // Print keys and values
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+
+        // To remove all items, use the clear() method:
+        capitalCities.clear();
     }
 }
 
 /*Expected Output:
 
 {USA=Washington DC, Norway=Oslo, England=London, Germany=Berlin}
+USA
+Norway
+Germany
+Washington DC
+Oslo
+Berlin
+key: USA value: Washington DC
+key: Norway value: Oslo
+key: Germany value: Berlin
 
 * */
