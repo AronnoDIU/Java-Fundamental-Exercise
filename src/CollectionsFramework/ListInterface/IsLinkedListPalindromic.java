0bd7c9d6d97c085dc5bc1b3dbaf6cc0148db6fcf
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-21 17:02:32 +0600

    PalindromeNode

---
+++ src/CollectionsFramework/ListInterface/IsLinkedListPalindromic.java
@@ -0,0 +1,62 @@
+package CollectionsFramework.ListInterface;
+
+public class IsLinkedListPalindromic {
+    PalindromeNode head;
+
+    boolean isPalindrome(PalindromeNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        PalindromeNode middle = findMiddle(head);
+        PalindromeNode secondHalfStart = reversedRecursive(middle.next);
+
+        PalindromeNode firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.val != secondHalfStart.val) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        return true;
+    }
+
+    // Reversed LinkedList
+    PalindromeNode reversedRecursive(PalindromeNode head) {
+        if (head == null || head.next == null) {    //Corner Point
+            return head;
+        }
+
+        PalindromeNode previousNode = null;
+        PalindromeNode currentNode = head;
+
+        while (currentNode != null) {
+            PalindromeNode next = currentNode.next;
+            currentNode.next = previousNode;
+
+            previousNode = currentNode;
+            currentNode = next;
+        }
+
+        return previousNode;
+    }
+
+    // Find Middle Node
+    PalindromeNode findMiddle(PalindromeNode head) {
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class PalindromeNode {
+    int val;
+    PalindromeNode next;
+
+    PalindromeNode(int val) {
+        this.val = val;
+    }
+}
