2e62164f3e3e0613638a5325df65884d4765ed69
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-23 10:58:48 +0600

    Stack from Scratch

---
+++ src/CollectionsFramework/ListInterface/VectorStack/StackScratchArrayList.java
@@ -0,0 +1,53 @@
+package CollectionsFramework.ListInterface.VectorStack;
+
+import java.util.ArrayList;
+
+public class StackScratchArrayList {
+    static class Stack {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        public void push(int data) {
+            list.add(data);
+        }
+
+        public boolean isEmpty() {
+            return list.isEmpty();
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.remove(list.size() - 1);
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
+
+/*Expected Output:
+
+4
+3
+2
+1
+
+* */
