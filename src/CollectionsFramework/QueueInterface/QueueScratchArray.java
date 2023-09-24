0dd17c8f65cb33acb5305ee6532454a3db384d43
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-24 06:53:30 +0600

    QueueScratchArray

---
+++ src/CollectionsFramework/QueueInterface/QueueScratchArray.java
@@ -0,0 +1,84 @@
+package CollectionsFramework.QueueInterface;
+
+public class QueueScratchArray {
+    static int[] arr;
+    static int size;
+    static int rear;
+
+
+    // Can't prevent instantiation of class!
+    QueueScratchArray(int size) {
+        QueueScratchArray.size = size;
+        arr = new int[size];
+        rear = -1;
+    }
+
+    public static boolean isEmpty() {
+        return rear == -1;
+    }
+
+    public static boolean isFull() {
+        return rear == size - 1;
+    }
+
+    public static void add(int data) {
+        if (isFull()) {
+            System.out.println("Overflow");
+            return;
+        }
+        arr[++rear] = data;
+    }
+
+    //O(n)
+    public static int remove() {
+        if (isEmpty()) {
+            System.out.println("empty queue");
+            return -1;
+        }
+        int front = arr[0];
+        for (int i = 0; i < rear; i++) {
+            arr[i] = arr[i + 1];
+        }
+        rear--;
+        return front;
+    }
+
+    public static int peek() {
+        if (isEmpty()) {
+            System.out.println("empty queue");
+            return -1;
+        }
+
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        new QueueScratchArray(5);
+        add(1);
+        add(2);
+        add(3);
+
+        while (!isEmpty()) {
+            System.out.println(peek());
+            remove();
+        }
+
+        System.out.println(); //There is no Queue left to remove
+        remove(); // removed from index[0] front!
+
+        while (!isEmpty()) { // empty queue
+            System.out.println(peek());
+            remove();
+        }
+    }
+}
+
+/*Expected Output:
+
+1
+2
+3
+
+empty queue
+
+* */
