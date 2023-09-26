c507c9c325976219427ad8acdea1bbc8d67a2c76
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-26 06:50:56 +0600

    Stacks using 2 Queue

--- src/CollectionsFramework/QueueInterface/QueueStacksEx2.java
+++ src/CollectionsFramework/QueueInterface/QueueStacksEx2.java
@@ -18,56 +18,72 @@ import java.util.Queue;
 // // Implement stack using two queues
 public class QueueStacksEx2 {
     public static void main(String[] args) {
+        int[] keys = { 1, 2, 3, 4, 5 };
 
+        // insert the above keys into the stack
+        StackQueue<Integer> s = new StackQueue<>();
+        for (int key: keys) {
+            s.add(key);
+        }
+
+        for (int i = 0; i <= keys.length; i++) {
+            System.out.println(s.poll());
+        }
     }
 }
 
-class Stack<T>
-{
-    Queue<T> q1, q2;
+class StackQueue<T> {
+    Queue<T> queue1, queue2;
 
     // Constructor
-    public Stack()
-    {
-        q1 = new ArrayDeque<>();
-        q2 = new ArrayDeque<>();
+    public StackQueue() {
+        queue1 = new ArrayDeque<>();
+        queue2 = new ArrayDeque<>();
     }
 
     // Insert an item into the stack
-    void add(T data)
-    {
+    void add(T data) {
         // Move all elements from the first queue to the second queue
-        while (!q1.isEmpty())
-        {
-            q2.add(q1.peek());
-            q1.poll();
+        while (!queue1.isEmpty()) {
+            queue2.add(queue1.peek());
+            queue1.poll();
         }
 
         // Push the given item into the first queue
-        q1.add(data);
+        queue1.add(data);
 
         // Move all elements back to the first queue from the second queue
-        while (!q2.isEmpty())
+        while (!queue2.isEmpty())
         {
-            q1.add(q2.peek());
-            q2.poll();
+            queue1.add(queue2.peek());
+            queue2.poll();
         }
     }
 
     // Remove the top item from the stack
-    public T poll()
-    {
+    public T poll() {
         // if the first queue is empty
-        if (q1.isEmpty())
+        if (queue1.isEmpty())
         {
             System.out.println("Underflow!!");
             System.exit(0);
         }
 
         // return the front item from the first queue
-        T front = q1.peek();
-        q1.poll();
+        T front = queue1.peek();
+        queue1.poll();
 
         return front;
     }
 }
+
+/*Expected Output:
+
+5
+4
+3
+2
+1
+Underflow!!
+
+* */
