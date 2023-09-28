0eff3a4fc025e31dfa1f7762f3019a7ebd230c62
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-29 05:48:05 +0600

    FloodFillAlgorithm DFS

---
+++ src/CollectionsFramework/QueueInterface/FloodFillAlgorithmEx2.java
@@ -0,0 +1,17 @@
+package CollectionsFramework.QueueInterface;
+
+/**
+ * Approach 2: (Using DFS)
+ * <p>
+ * We can use Depth–first search (DFS) to solve this problem. The idea is to start
+ * from the source node in the matrix, replace its color with the replacement color
+ * and recursively explore all its valid eight adjacent pixels, and replace their
+ * color. Note that we don’t need a visited array here as we are replacing the color
+ * of every processed node, and it won’t be considered again next time as it will
+ * have a different color.
+ */
+public class FloodFillAlgorithmEx2 {
+    public static void main(String[] args) {
+
+    }
+}
