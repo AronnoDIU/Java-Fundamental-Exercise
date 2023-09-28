6b5ab3347fac4483f3f68d063918bf5a6befe04f
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-28 17:41:56 +0600

    Efficiently print all nodes between two given levels in a binary tree

--- src/CollectionsFramework/QueueInterface/PrintNodesBinaryTree.java
+++ src/CollectionsFramework/QueueInterface/PrintNodesBinaryTree.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.QueueInterface;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Given a binary tree, efficiently print all nodes in it between two given levels.
  * The nodes for any level should be printed from the left and right.
@@ -38,15 +41,77 @@ package CollectionsFramework.QueueInterface;
  * size = size – 1
  */
 public class PrintNodesBinaryTree {
-    public static void main(String[] args) {
+    // Iterative function to print all nodes between two given
+    // levels in a binary tree
+    static void printNodes(Nodes root, int start, int end) {
+        if (root == null) {
+            return;
+        }
 
+        // create an empty queue and enqueue the root node
+        Queue<Nodes> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        // to store the current node
+        Nodes curr = null;
+
+        // maintains the level of the current node
+        int level = 0;
+
+        // loop till queue is empty
+        while (!queue.isEmpty()) {
+            // increment level by 1
+            level++;
+
+            // calculate the total number of nodes at the current level
+            int size = queue.size();
+
+            // process every node of the current level and enqueue their
+            // non-empty left and right child
+            while (size-- > 0) {
+                curr = queue.poll();
+
+                // print the node if its level is between given levels
+                if (level >= start && level <= end) {
+                    assert curr != null;
+                    System.out.print(curr.key + " ");
+                }
+
+                assert curr != null;
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+
+            if (level >= start && level <= end) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Nodes root = new Nodes(15);
+        root.left = new Nodes(10);
+        root.right = new Nodes(20);
+        root.left.left = new Nodes(8);
+        root.left.right = new Nodes(12);
+        root.right.left = new Nodes(16);
+        root.right.right = new Nodes(25);
+        root.right.right.right = new Nodes(30);
+
+        int start = 2, end = 3;
+        printNodes(root, start, end);
     }
 }
 
 // A class to store a binary tree node
 class Nodes {
     int key;
-    Node left = null, right = null;
+    Nodes left = null, right = null;
 
     Nodes(int key) {
         this.key = key;
