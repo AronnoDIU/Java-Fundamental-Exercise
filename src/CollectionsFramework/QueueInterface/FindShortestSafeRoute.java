4b5e622e75e845155319c46cf651636e073672e7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-02 10:49:58 +0600

    Find the shortest safe route in a field with sensors present

---
+++ src/CollectionsFramework/QueueInterface/FindShortestSafeRoute.java
@@ -0,0 +1,8 @@
+package CollectionsFramework.QueueInterface;
+
+// Find the shortest safe route in a field with sensors present
+public class FindShortestSafeRoute {
+    public static void main(String[] args) {
+
+    }
+}
