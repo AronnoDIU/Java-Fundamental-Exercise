18f99bb3ee8bac14b378f71920a8633156117012
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-04 21:59:42 +0600

    Print left view of a binary tree

--- src/CollectionsFramework/QueueInterface/PrintLeftViewBinaryTree.java
+++ src/CollectionsFramework/QueueInterface/PrintLeftViewBinaryTree.java
@@ -1,11 +1,54 @@
 package CollectionsFramework.QueueInterface;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**1. Iterative Implementation
 
  In the iterative version, perform a level order traversal on the tree.
  We can modify level order traversal to maintain nodes at the current level.
  Then, if the current node is the first node of the current level, print it.*/
 public class PrintLeftViewBinaryTree {
+    // Iterative function to print the left view of a given binary tree
+    public static void leftView(NodePLVBT root) {
+        // return if the tree is empty
+        if (root == null) {
+            return;
+        }
+
+        // create an empty queue and enqueue the root node
+        Queue<NodePLVBT> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        // to store the current node
+        NodePLVBT curr;
+
+        // loop till queue is empty
+        while (!queue.isEmpty()) {
+            // calculate the total number of nodes at the current level
+            int size = queue.size();
+            int i = 0;
+
+            // process every node of the current level and enqueue their
+            // non-empty left and right child
+            while (i++ < size) {
+                curr = queue.poll();
+
+                // if this is the first node of the current level, print it
+                if (i == 1) {
+                    System.out.print(curr.key + " ");
+                }
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
 
     }
