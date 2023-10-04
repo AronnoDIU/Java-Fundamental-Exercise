aca3f8754a2af1d4495aaca01c80ab22365a2ecc
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-04 21:57:23 +0600

    Print left view of a binary tree

--- src/CollectionsFramework/QueueInterface/PrintLeftViewBinaryTree.java
+++ src/CollectionsFramework/QueueInterface/PrintLeftViewBinaryTree.java
@@ -1,5 +1,10 @@
 package CollectionsFramework.QueueInterface;
 
+/**1. Iterative Implementation
+
+ In the iterative version, perform a level order traversal on the tree.
+ We can modify level order traversal to maintain nodes at the current level.
+ Then, if the current node is the first node of the current level, print it.*/
 public class PrintLeftViewBinaryTree {
     public static void main(String[] args) {
 
