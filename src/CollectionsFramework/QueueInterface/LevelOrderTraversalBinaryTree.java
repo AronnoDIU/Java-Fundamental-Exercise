9c933152a61b6325ebd2fd1cfddb71d0a51d056d
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-05 09:57:03 +0600

    Level order traversal of a binary tree

--- src/CollectionsFramework/QueueInterface/LevelOrderTraversalBinaryTree.java
+++ src/CollectionsFramework/QueueInterface/LevelOrderTraversalBinaryTree.java
@@ -1,5 +1,20 @@
 package CollectionsFramework.QueueInterface;
 
+/**
+ * We have already discussed preorder, inorder and postorder traversals of the binary tree,
+ * which are nothing but variations of Depth–first search of a Tree.
+ * Trees can also be traversed in level order, where we visit every node
+ * on a level before going to a lower level.
+ * This search is referred to as level order traversal or Breadth - first search (BFS),
+ * as the search tree is broadened as much as possible on each depth before going
+ * to the next depth.
+ * <p>
+ * <p>
+ * A simple solution is to print all nodes of level 1 first, followed by level 2,
+ * until level h, where h is the tree’s height.
+ * We can print all nodes present in a level by modifying the preorder traversal
+ * on the tree.
+ */
 // Level order traversal of a binary tree
 public class LevelOrderTraversalBinaryTree {
     public static void main(String[] args) {
