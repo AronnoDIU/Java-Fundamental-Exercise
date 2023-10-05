8dcb31e598175c5e89b38da1bf78d59a7efcaa2c
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-05 09:49:45 +0600

    Print corner nodes of every level in a binary tree

--- src/CollectionsFramework/QueueInterface/PrintCornerNodes.java
+++ src/CollectionsFramework/QueueInterface/PrintCornerNodes.java
@@ -1,5 +1,9 @@
 package CollectionsFramework.QueueInterface;
 
+/**First, modify the level order traversal on a given binary tree to maintain the
+ * level of each node. Then while doing level order traversal,
+ * if the current node happens to be the first or last
+ * node at the current level, print it.*/
 // Print corner nodes of every level in a binary tree
 public class PrintCornerNodes {
     public static void main(String[] args) {
