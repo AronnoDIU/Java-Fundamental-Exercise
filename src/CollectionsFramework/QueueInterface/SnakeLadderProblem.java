eadf0c393d4ebf66828381dc3c74f54a52cc1df2
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-06 16:09:33 +0600

    Snake and Ladder Problem

--- src/CollectionsFramework/QueueInterface/SnakeLadderProblem.java
+++ src/CollectionsFramework/QueueInterface/SnakeLadderProblem.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.QueueInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The idea is to consider the snake and ladders board as a directed graph and run
  * Breadth–first search (BFS) from the starting node, vertex 0, as per game rules.
@@ -19,5 +22,49 @@ package CollectionsFramework.QueueInterface;
 public class SnakeLadderProblem {
     public static void main(String[] args) {
 
+    }
+}
+
+// A class to store a graph edge
+class EdgeSLP {
+    int src, dest;
+
+    public EdgeSLP(int src, int dest) {
+        this.src = src;
+        this.dest = dest;
+    }
+}
+
+// A queue node
+class NodeSLP {
+    // stores number associated with graph node
+    int ver;
+
+    // `min_dist` stores the minimum distance of a node from the starting vertex
+    int min_dist;
+
+    public NodeSLP(int ver, int min_dist) {
+        this.ver = ver;
+        this.min_dist = min_dist;
+    }
+}
+
+// A class to represent a graph object
+class GraphNLP {
+    // A list of lists to represent an adjacency list
+    List<List<Integer>> adjList = null;
+
+    // Constructor
+    GraphNLP(List<EdgeSLP> edges, int n) {
+        adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // add edges to the graph
+        for (EdgeSLP edge : edges) {
+            // Please note that the graph is directed
+            adjList.get(edge.src).add(edge.dest);
+        }
     }
 }
