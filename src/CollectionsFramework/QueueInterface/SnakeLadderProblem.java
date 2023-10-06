1143c0e5df2cd6563fc49d8157851e37ea343021
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-06 16:28:33 +0600

    Snake and Ladder Problem

--- src/CollectionsFramework/QueueInterface/SnakeLadderProblem.java
+++ src/CollectionsFramework/QueueInterface/SnakeLadderProblem.java
@@ -98,7 +98,31 @@ public class SnakeLadderProblem {
     }
 
     public static void main(String[] args) {
+        // snakes and ladders are represented using a map.
+        Map<Integer, Integer> ladder = new HashMap<>();
+        Map<Integer, Integer> snake = new HashMap<>();
 
+        // insert ladders into the map
+        ladder.put(1, 38);
+        ladder.put(4, 14);
+        ladder.put(9, 31);
+        ladder.put(21, 42);
+        ladder.put(28, 84);
+        ladder.put(51, 67);
+        ladder.put(72, 91);
+        ladder.put(80, 99);
+
+        // insert snakes into the map
+        snake.put(17, 7);
+        snake.put(54, 34);
+        snake.put(62, 19);
+        snake.put(64, 60);
+        snake.put(87, 36);
+        snake.put(93, 73);
+        snake.put(95, 75);
+        snake.put(98, 79);
+
+        System.out.println(findMinimumMoves(ladder, snake));
     }
 }
 
@@ -129,7 +153,7 @@ class NodeSLP {
 // A class to represent a graph object
 class GraphSLP {
     // A list of lists to represent an adjacency list
-    List<List<Integer>> adjacentList = null;
+    List<List<Integer>> adjacentList;
 
     // Constructor
     GraphSLP(List<EdgeSLP> edgeSLPList, int number) {
