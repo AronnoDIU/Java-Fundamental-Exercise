3f8b26910092f5697ff1fc2327039f5644489a30
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-07 15:12:09 +0600

    Check if a binary tree is a complete binary tree or not Array Rep

--- src/CollectionsFramework/QueueInterface/CheckBinaryTreeArrayRep.java
+++ src/CollectionsFramework/QueueInterface/CheckBinaryTreeArrayRep.java
@@ -11,6 +11,53 @@ package CollectionsFramework.QueueInterface;
  */
 // 2. Array representation of a complete tree
 public class CheckBinaryTreeArrayRep {
+    // Utility function to calculate the total number of nodes in a binary tree
+    private static int size(NodeCBTAR root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    // Perform inorder traversal on the binary tree and fill array `A[]`
+    static void inorder(NodeCBTAR root, boolean[] A, int i) {
+        if (root == null || i >= A.length) {
+            return;
+        }
+
+        // recur with index `2i+1` for left node
+        inorder(root.left, A, 2 * i + 1);
+
+        // mark index `i` as visited
+        A[i] = true;
+
+        // recur with index `2i+2` for the right node
+        inorder(root.right, A, 2 * i + 2);
+    }
+
+    // Function to check if a given binary tree is a complete binary tree or not
+    static boolean isComplete(NodeCBTAR root, int n) {
+        // return if the tree is empty
+        if (root == null) {
+            return true;
+        }
+
+        // construct an auxiliary boolean array of size `n`
+        boolean[] A = new boolean[n];
+
+        // fill array `A[]`
+        inorder(root, A, 0);
+
+        // check if all positions in the array are filled or not
+        for (boolean e : A) {
+            if (!e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
     }
