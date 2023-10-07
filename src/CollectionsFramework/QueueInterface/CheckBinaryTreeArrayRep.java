cc86bdde274ee520b7533f0738b3db3914cbdbf8
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-07 15:10:23 +0600

    Check if a binary tree is a complete binary tree or not Array Rep

---
+++ src/CollectionsFramework/QueueInterface/CheckBinaryTreeArrayRep.java
@@ -0,0 +1,17 @@
+package CollectionsFramework.QueueInterface;
+
+/**
+ * We can solve this problem by using the properties of a complete binary tree.
+ * We know that in the array representation of a binary tree,
+ * the left child for a node at index i is present at index 2i+1, and the right
+ * child is present at index 2i+2. If we construct an array with all the tree
+ * elements at the corresponding positions, then the elements will hold
+ * consecutive positions for a complete binary tree. If any vacant position
+ * is found, then the tree cannot be complete.
+ */
+// 2. Array representation of a complete tree
+public class CheckBinaryTreeArrayRep {
+    public static void main(String[] args) {
+
+    }
+}
