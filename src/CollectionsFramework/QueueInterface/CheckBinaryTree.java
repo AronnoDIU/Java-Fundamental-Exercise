7e5f1641eea46fbac47c980bdbaa821869c63f06
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-07 15:02:12 +0600

    Check if a binary tree is a complete binary tree or not

---
+++ src/CollectionsFramework/QueueInterface/CheckBinaryTree.java
@@ -0,0 +1,8 @@
+package CollectionsFramework.QueueInterface;
+
+// Check if a binary tree is a complete binary tree or not
+public class CheckBinaryTree {
+    public static void main(String[] args) {
+
+    }
+}
