d0131e910ea9d7c8902fb9a0f0a8150f6a50fb0a
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-07 15:04:00 +0600

    Check if a binary tree is a complete binary tree or not

--- src/CollectionsFramework/QueueInterface/CheckBinaryTree.java
+++ src/CollectionsFramework/QueueInterface/CheckBinaryTree.java
@@ -1,5 +1,17 @@
 package CollectionsFramework.QueueInterface;
 
+/**
+ * 1. Level Order Traversal (BFS)
+ * <p>
+ * We can modify level order traversal to check if a given
+ * binary tree is a complete binary tree or not.
+ * The idea is for every dequeued node, check if
+ * it is a full node (have both left and right children).
+ * If a node is found that is not a full node,
+ * i.e., either it has no children or only one child,
+ * then all the remaining nodes in the queue should not have any children.
+ * If anyone has a child, then it’s not a complete binary tree; otherwise, it is.
+ */
 // Check if a binary tree is a complete binary tree or not
 public class CheckBinaryTree {
     public static void main(String[] args) {
