131f623aa1df2d07de1910634801f04ac4b820a7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-08 13:23:46 +0600

    Check if a binary tree is a complete binary tree or not Space-optimized

--- src/CollectionsFramework/QueueInterface/CheckBinaryTreeSpaceOpt.java
+++ src/CollectionsFramework/QueueInterface/CheckBinaryTreeSpaceOpt.java
@@ -1,5 +1,15 @@
 package CollectionsFramework.QueueInterface;
 
+/**
+ * 3. Space-optimized previous Approach
+ * <p>
+ * The above approach takes extra space for storage of the boolean array.
+ * As discussed for a complete binary tree,the left and right child’s index
+ * for any node is less than the total number of nodes for every node.
+ * We can avoid using extra space by passing the index as a recursion parameter
+ * and checking for every node that their left and right child’s
+ * index is within the correct range.
+ */
 // Space-optimized previous Approach
 public class CheckBinaryTreeSpaceOpt {
     public static void main(String[] args) {
