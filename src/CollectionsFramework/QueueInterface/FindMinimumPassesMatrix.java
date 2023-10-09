91f495968dbca89db1b8f574114e50ef7ceea3b6
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-09 06:03:15 +0600

    Find minimum passes required to convert all negative values in a matrix

---
+++ src/CollectionsFramework/QueueInterface/FindMinimumPassesMatrix.java
@@ -0,0 +1,8 @@
+package CollectionsFramework.QueueInterface;
+
+// Find minimum passes required to convert all negative values in a matrix
+public class FindMinimumPassesMatrix {
+    public static void main(String[] args) {
+
+    }
+}
