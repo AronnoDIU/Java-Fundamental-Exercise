3bb9462f837f071a624ddea04992243bf40a0a5b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-10 14:48:12 +0600

    Build Tree from given Preorder Sequence

--- src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
+++ src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
@@ -96,6 +96,111 @@ public class BuildTreePreorderSequence {
         }
     }
 
+    //    3. Height of Tree
+    static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    //    4. Count of Nodes of Tree
+    static int countOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        return leftNodes + rightNodes + 1;
+    }
+
+    //    5. Sum of Nodes of Tree
+    static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+    //    6. Diameter of Tree - Approach1 O(N^2)
+    static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        int diam1 = height(root.left) + height(root.right) + 1;
+        int diam2 = diameter(root.left);
+        int diam3 = diameter(root.right);
+
+
+        return Math.max(diam1, Math.max(diam2, diam3));
+    }
+
+//    7. Diameter of Tree - Approach2 O(N)
+     /*static TreeInfo diameter(Node root) {
+        if(root == null) {
+            return new TreeInfo(0, 0);
+        }
+
+
+        TreeInfo leftTI = diameter(root.left);
+        TreeInfo rightTI = diameter(root.right);
+
+
+        int myHeight = Math.max(leftTI.height, rightTI.height) + 1;
+
+
+        Int diam1 = leftTI.height + rightTI.height + 1;
+        int diam2 = leftTI.diam;
+        int diam3 = rightTI.diam;
+
+
+        Int myDiam = Math.max(diam1, Math.max(diam2, diam3));
+
+
+        return new TreeInfo(myHeight, myDiam);
+    }*/
+
+//    8. Subtree of another tree
+
+    /*public boolean isIdentical(TreeNode root,TreeNode subRoot){
+        if(subRoot == null && root == null){
+            return true;
+        }
+        if(root == null || subRoot == null){
+            return false;
+        }
+        if(root.val == subRoot.val){
+            return isIdentical(root.left, subRoot.left) &&
+                    isIdentical(root.right, subRoot.right);
+        }
+        return false;
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(subRoot == null){
+            return true;
+        }
+        if(root == null){
+            return false;
+        }
+        if(isIdentical(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }*/
+
     public static void main(String[] args) {
         // Tree data given from Preorder Sequence
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
