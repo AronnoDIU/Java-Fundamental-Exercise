64debec5466d714f8714f95d2eb6c8cf78d8ac01
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-10 14:42:27 +0600

    Build Tree from given Preorder Sequence

--- src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
+++ src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
@@ -35,66 +35,67 @@ public class BuildTreePreorderSequence {
     /*Tree Traversals*/
 //    Preorder -> root -> left -> right
     static void preorder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preorder(root.left);
         preorder(root.right);
     }
 
-//    Inorder
+    //    Inorder
     static void inorder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
             return;
         }
         inorder(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
 
-//    Postorder
+    //    Postorder
     static void postorder(Node root) {
-        if(root == null) {
-            System.out.print(-1+" ");
+        if (root == null) {
+            System.out.print(-1 + " ");
             return;
         }
         postorder(root.left);
         postorder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
 
-//     Level Order
+    //     Level Order
     static void levelOrder(Node root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
-        while(!q.isEmpty()) {
-            Node curr = q.remove();
-            if(curr == null) {
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        nodeQueue.add(null);
+        while (!nodeQueue.isEmpty()) {
+            Node curr = nodeQueue.remove();
+            if (curr == null) {
                 System.out.println();
                 //queue empty
-                if(q.isEmpty()) {
+                if (nodeQueue.isEmpty()) {
                     break;
                 } else {
-                    q.add(null);
+                    nodeQueue.add(null);
                 }
             } else {
-                System.out.print(curr.data+" ");
-                if(curr.left != null) {
-                    q.add(curr.left);
+                System.out.print(curr.data + " ");
+                if (curr.left != null) {
+                    nodeQueue.add(curr.left);
                 }
-                if(curr.right != null) {
-                    q.add(curr.right);
+                if (curr.right != null) {
+                    nodeQueue.add(curr.right);
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         // Tree data given from Preorder Sequence
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
