ed578ade1ba628b824cf62c6796b96148c6c7f29
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-10 12:37:16 +0600

    Build Tree from given Preorder Sequence

--- src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
+++ src/CollectionsFramework/SetInterface/SortedSetInterface/BuildTreePreorderSequence.java
@@ -7,7 +7,6 @@ public class BuildTreePreorderSequence {
         Node left;
         Node right;
 
-
         Node(int data) {
             this.data = data;
             this.left = null;
@@ -31,7 +30,9 @@ public class BuildTreePreorderSequence {
     }
 
     public static void main(String[] args) {
+        // Tree data given from Preorder Sequence
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
 //        BinaryTree tree = new BinaryTree();
 
         Node root = BinaryTree.buildTree(nodes);
