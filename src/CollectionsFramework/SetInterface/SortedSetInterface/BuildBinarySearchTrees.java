e594c4a7d16c51e86073b2bafc91535df1c682f3
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-12 23:13:09 +0600

    Build Binary Search Trees

--- src/CollectionsFramework/SetInterface/SortedSetInterface/BuildBinarySearchTrees.java
+++ src/CollectionsFramework/SetInterface/SortedSetInterface/BuildBinarySearchTrees.java
@@ -1,7 +1,33 @@
 package CollectionsFramework.SetInterface.SortedSetInterface;
 
 public class BuildBinarySearchTrees {
-    public static void main(String[] args) {
+    static class NodeBST {
+        int data;
+        NodeBST left;
+        NodeBST right;
 
+        public NodeBST(int data) {
+            this.data = data;
+        }
+    }
+
+    static NodeBST insert(NodeBST root, int key) {
+        if (root == null) {
+            root = new NodeBST(key);
+            return root;
+        }
+
+        if (root.data > key) {
+            // left subtree
+            root.left = insert(root.left, key);
+        } else {
+            root.right = insert(root.right, key);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] values = {5, 1, 3, 4, 2, 7};
+        NodeBST root = null;
     }
 }
