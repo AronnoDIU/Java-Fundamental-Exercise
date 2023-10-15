f399bf9228bf41848ed5c25cbd0259f35d45d279
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-15 07:13:34 +0600

    Hashing

--- src/CollectionsFramework/SetInterface/HashSetEx2.java
+++ src/CollectionsFramework/SetInterface/HashSetEx2.java
@@ -1,7 +1,55 @@
 package CollectionsFramework.SetInterface;
 
+import java.util.HashSet;
+
 public class HashSetEx2 {
     public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
 
+        //Add
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+
+        //Size
+        System.out.println("size of set is : " + set.size());
+
+
+        //Search
+        if (set.contains(1)) {
+            System.out.println("present");
+        }
+
+
+        if (!set.contains(6)) {
+            System.out.println("absent");
+        }
+
+
+        //Delete
+        set.remove(1);
+        if (!set.contains(1)) {
+            System.out.println("absent");
+        }
+
+
+        //Print all elements
+        System.out.println(set);
+
+
+        //Iteration - HashSet does not have an order
+        set.add(0);
+        for (Integer integer : set) {
+            System.out.print(integer + ", ");
+        }
+        System.out.println();
+
+
+        //isEmpty
+        if (!set.isEmpty()) {
+            System.out.println("set is not empty");
+        }
     }
 }
