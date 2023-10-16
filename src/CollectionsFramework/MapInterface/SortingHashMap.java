de0b666dd9fd3b55bc51393abd59af308017fa50
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-16 19:01:56 +0600

    Sorting a HashMap according to keys

--- src/CollectionsFramework/MapInterface/SortingHashMap.java
+++ src/CollectionsFramework/MapInterface/SortingHashMap.java
@@ -1,6 +1,26 @@
 package CollectionsFramework.MapInterface;
 
 // Sorting a HashMap according to keys in Java
+/**
+ * We are given the details of marks scored by students in the form of a HashMap,
+ * where the name of the student is the Key and the marks scored is the Value.
+ * Our task is to sort the map according to the key values, i.e.,
+ * the names of the students in the alphabetical(lexicographical) order.
+ * Examples:
+ * <p>
+ * Input :
+ * Key = Jayanti, Value = 80
+ * Key = Anushka, Value = 80
+ * Key = Amit, Value = 75
+ * Key = Abhilasha, Value = 90
+ * Key = Danish, Value = 40
+ * Output : Sorted Map according to Names:
+ * Key = Abhilasha, Value = 90
+ * Key = Amit, Value = 75
+ * Key = Anushka, Value = 80
+ * Key = Danish, Value = 40
+ * Key = Jayanti, Value = 80
+ */
 public class SortingHashMap {
     public static void main(String[] args) {
 
