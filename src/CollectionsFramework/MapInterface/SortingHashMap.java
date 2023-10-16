352d588b8a0204682e8eb5b9a04e955054ce6bb7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-16 19:07:45 +0600

    Sorting a HashMap according to keys

--- src/CollectionsFramework/MapInterface/SortingHashMap.java
+++ src/CollectionsFramework/MapInterface/SortingHashMap.java
@@ -1,6 +1,11 @@
 package CollectionsFramework.MapInterface;
 
 // Sorting a HashMap according to keys in Java
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * We are given the details of marks scored by students in the form of a HashMap,
  * where the name of the student is the Key and the marks scored is the Value.
@@ -22,7 +27,30 @@ package CollectionsFramework.MapInterface;
  * Key = Jayanti, Value = 80
  */
 public class SortingHashMap {
-    public static void main(String[] args) {
+    // This map stores unsorted values
+    static Map<String, Integer> map = new HashMap<>();
 
+    // Function to sort a map by Key
+    public static void sortByKey() {
+        // TreeMap to store values of HashMap
+        TreeMap<String, Integer> sorted
+                = new TreeMap<>(map);
+
+        // Display the TreeMap which is naturally sorted
+        for (Map.Entry<String, Integer> entry : sorted.entrySet())
+            System.out.println("Key = " + entry.getKey()
+                    + ", Value = " + entry.getValue());
+    }
+
+    public static void main(String[] args) {
+        // putting values in the Map
+        map.put("Jayanti", 80);
+        map.put("Abhilasha", 90);
+        map.put("Anushka", 80);
+        map.put("Amit", 75);
+        map.put("Danish", 40);
+
+        // Calling the function to sortByKey
+        sortByKey();
     }
 }
