8eb1d40ad07ddbb959ff53ee2adab58989fa7076
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-18 18:04:31 +0600

    MajorityElement using HashMap

--- src/CollectionsFramework/MapInterface/MajorityElement.java
+++
@@ -1,8 +0,0 @@
-package CollectionsFramework.MapInterface;
-
-// Given an Integer Array of size N, find all elements that appear more than[N/3]times.
-public class MajorityElement {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/CollectionsFramework/MapInterface/MajorityElementHashMap.java
@@ -0,0 +1,37 @@
+package CollectionsFramework.MapInterface;
+
+import java.util.HashMap;
+
+/**
+ * Given an Integer Array of size N, find all elements that appear more than[N/3]times.
+ * 3-step processes,
+ * 1. Create HashMap Object.
+ * 2. Find frequency of all numbers & store in Map.
+ * 3. Travers the HashMap Object & find which elements more than N/3.
+ */
+public class MajorityElementHashMap {
+    static void majorityElements(int[] Numbers) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        // Find frequency of all numbers & store in Map
+        int N = Numbers.length;
+        for (int numberIndex : Numbers) {
+            if (hashMap.containsKey(numberIndex)) {
+                hashMap.put(numberIndex, hashMap.get(numberIndex + 1));
+            } else {
+                hashMap.put(numberIndex, 1);
+            }
+        }
+
+        for (int KeyIndex : hashMap.keySet()) {
+            if (hashMap.get(KeyIndex) > N / 3) {
+                System.out.println(KeyIndex);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] Numbers = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        majorityElements(Numbers);
+    }
+}
