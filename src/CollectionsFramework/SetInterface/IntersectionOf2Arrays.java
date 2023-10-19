569514c62a579799c020d4d0e34e064aaa25b367
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-19 19:59:00 +0600

    Intersection of two Arrays

--- src/CollectionsFramework/SetInterface/IntersectionOf2Arrays.java
+++ src/CollectionsFramework/SetInterface/IntersectionOf2Arrays.java
@@ -1,9 +1,34 @@
 package CollectionsFramework.SetInterface;
 
+import java.util.HashSet;
+
 // Intersection of two Arrays
 // Array1[] = {1, 2, 3} && Array2[] = {1, 4, 5, 3} ==> Intersection[] = 2 {1, 3}.
 public class IntersectionOf2Arrays {
-    public static void main(String[] args) {
+    public static int intersectionElements(int[] Array1, int[] Array2) {
+        HashSet<Integer> integerHashSet = new HashSet<>();
+        int count = 0;
 
+        // Add Array1 elements to HashSet
+        for (int Array1Index : Array1) {
+            integerHashSet.add(Array1Index);
+        }
+
+        // // Add Array2 elements to HashSet
+        for (int Array2Index : Array2) {
+            if (integerHashSet.contains(Array2Index)) {
+                count++;
+                integerHashSet.remove(Array2Index);
+            }
+        }
+        return count;   // Intersection
+    }
+
+    public static void main(String[] args) {
+        int[] Array1 = {7, 3, 9};
+        int[] Array2 = {6, 3, 9, 2, 9, 4};
+
+        System.out.println("Total Number of Intersection: "
+                + intersectionElements(Array1, Array2));    // 2
     }
 }
