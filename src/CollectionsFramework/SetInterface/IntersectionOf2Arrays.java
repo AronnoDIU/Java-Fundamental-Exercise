2e06320a18dcf1f97b1d16bd6a08b5d84599d281
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-19 13:22:32 +0600

    Intersection of two Arrays

--- src/CollectionsFramework/SetInterface/IntersectionOf2Arrays.java
+++ src/CollectionsFramework/SetInterface/IntersectionOf2Arrays.java
@@ -1,6 +1,7 @@
 package CollectionsFramework.SetInterface;
 
 // Intersection of two Arrays
+// Array1[] = {1, 2, 3} && Array2[] = {1, 4, 5, 3} ==> Intersection[] = 2 {1, 3}.
 public class IntersectionOf2Arrays {
     public static void main(String[] args) {
 
