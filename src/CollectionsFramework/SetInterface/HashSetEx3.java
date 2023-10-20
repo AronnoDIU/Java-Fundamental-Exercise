66307304ad4b9ab64c5f8bab6d91bb6ba09bbd24
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-20 22:08:37 +0600

    Adding books to HashSet and print

--- src/CollectionsFramework/SetInterface/HashSetEx3.java
+++ src/CollectionsFramework/SetInterface/HashSetEx3.java
@@ -7,13 +7,13 @@ public class HashSetEx3 {
     public static void main(String[] args) {
         HashSet<Book> bookHashSet= new HashSet<>();
         //Creating Books
-        Book b1=new Book(101,"Let us C","Yashwant Kanetkar","BPB",8);
-        Book b2=new Book(102,"Data Communications & Networking","Forouzan","Mc Graw Hill",4);
-        Book b3=new Book(103,"Operating System","Galvin","Wiley",6);
+        Book book1=new Book(101,"Let us C","Washstand Karabiner","BPB",8);
+        Book book2=new Book(102,"Data Communications & Networking","Forough","Mc Graw Hill",4);
+        Book book3=new Book(103,"Operating System","Galvin","Wiley",6);
         //Adding Books to HashSet
-        bookHashSet.add(b1);
-        bookHashSet.add(b2);
-        bookHashSet.add(b3);
+        bookHashSet.add(book1);
+        bookHashSet.add(book2);
+        bookHashSet.add(book3);
         //Traversing HashSet
         for(Book b:bookHashSet){
             System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);
