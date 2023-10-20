a3fad7962695cccd0431c7069f8189313f837b2b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-20 22:05:40 +0600

    Adding books to HashSet and print

--- src/CollectionsFramework/SetInterface/HashSetEx3.java
+++ src/CollectionsFramework/SetInterface/HashSetEx3.java
@@ -6,3 +6,16 @@ public class HashSetEx3 {
 
     }
 }
+
+class Book {
+    int id;
+    String name,author,publisher;
+    int quantity;
+    public Book(int id, String name, String author, String publisher, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.quantity = quantity;
+    }
+}
