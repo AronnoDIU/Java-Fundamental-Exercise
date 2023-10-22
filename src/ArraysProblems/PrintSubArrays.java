4a4ca6baa8d8aef6eeee1b4e932e099c857019a1
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-22 08:20:31 +0600

    Print Sub-Array

--- src/ArraysProblems/PrintSubArrays.java
+++ src/ArraysProblems/PrintSubArrays.java
@@ -3,15 +3,19 @@ package ArraysProblems;
 // Print Sub-Array ==> Continuation of Arrays -> like, 2,4,6 not like, 2,4,8
 public class PrintSubArrays {
     static void printSubArrays(int[] Numbers) {
+        int TotalSubArrays = 0; // Sub-Arrays == (N(N+1))/2;
+
         for (int i = 0; i < Numbers.length; i++) {
             for (int j = i; j < Numbers.length; j++) {
                 for (int k = i; k <= j; k++) {    // Print Only
                     System.out.print(Numbers[k] + " "); // SubArray
                 }
+                TotalSubArrays++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total Sub-Arrays = " + TotalSubArrays);
     }
 
     public static void main(String[] args) {
@@ -41,5 +45,7 @@ public class PrintSubArrays {
 8 10
 
 10
+
+Total Sub-Arrays = 15
 
 * */
