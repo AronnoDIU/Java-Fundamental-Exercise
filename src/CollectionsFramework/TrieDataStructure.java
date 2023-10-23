5e08fce363ef394fb82391f7e72d36062b73c8be
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-23 06:34:16 +0600

    Trie Data Structure Implementation

--- src/CollectionsFramework/TrieDataStructure.java
+++ src/CollectionsFramework/TrieDataStructure.java
@@ -18,7 +18,71 @@ package CollectionsFramework;
  * 3. Tries help with the longest prefix matching, when we want to find the key.
  */
 public class TrieDataStructure {
+    static class NodeTDS {
+        NodeTDS[] children = new NodeTDS[26];
+        boolean endOfWord;
+
+        public NodeTDS() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+//            endOfWord=false;
+        }
+    }
+
+    static NodeTDS root = new NodeTDS();
+
+
+    public static void insert(String word) { //O(n)
+        int level = 0;
+        int len = word.length();
+        int idx = 0;
+
+
+        NodeTDS curr = root;
+        for (; level < len; level++) {
+            idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new NodeTDS();
+            }
+            curr = curr.children[idx];
+        }
+        curr.endOfWord = true;
+    }
+
+    public static boolean search(String key) { //O(n)
+        int level = 0;
+        int len = key.length();
+        int idx = 0;
+
+
+        NodeTDS curr = root;
+        for (; level < len; level++) {
+            idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.endOfWord == true;
+    }
+
+
     public static void main(String[] args) {
+        String[] words = {"the", "a", "there", "their", "any", "thee"};
+
+        for (String word : words) {
+            insert(word);
+            System.out.println("inserted " + word);
+        }
+
+
+        System.out.println("thee -> " + search("thee"));
+        System.out.println("thor -> " + search("thor"));
+
+
+//        System.out.println(startsWith("the"));
+//        System.out.println(startsWith("thi"));
 
     }
 }
