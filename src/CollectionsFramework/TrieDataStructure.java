ae226bb5e1af449f85bba32dd48e6c8f3a18ecd4
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-23 06:23:09 +0600

    Trie Data Structure Implementation

---
+++ src/CollectionsFramework/TrieDataStructure.java
@@ -0,0 +1,24 @@
+package CollectionsFramework;
+
+/**
+ * What is Trie?
+ * <p>
+ * Trie is a type of k-ary search tree used for storing and searching a
+ * specific key from a set. Using Trie, search complexities can be brought
+ * to optimal limit (key length).
+ * <p>
+ * Advantages of tries
+ * <p>
+ * 1. In tries the keys are searched using common prefixes. Hence, it is faster.
+ * The lookup of keys depends upon the height in case of a binary search tree.
+ * <p>
+ * 2. Tries to take less space when they contain a large number of short strings.
+ * As nodes are shared between the keys.
+ * <p>
+ * 3. Tries help with the longest prefix matching, when we want to find the key.
+ */
+public class TrieDataStructure {
+    public static void main(String[] args) {
+
+    }
+}
