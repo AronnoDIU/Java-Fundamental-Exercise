ec8014ee53485fa21cf7cdc1cd3f8b0b00f8509c
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-24 15:17:30 +0600

    StartsWith using Trie DS

---
+++ src/CollectionsFramework/StartsWithTrieDS.java
@@ -0,0 +1,15 @@
+package CollectionsFramework;
+
+/*Create a function boolean startsWith(String prefix) for a trie.
+ * Returns true if there is a previously inserted string word that
+ * has the prefix prefix, and false otherwise. */
+public class StartsWithTrieDS {
+    public static void main(String[] args) {
+        String[] words = {"apple", "app", "mango", "man", "woman"};
+
+//        for (String wordList : words) {
+//            insert(wordList);
+//            System.out.println("inserted " + wordList);
+//        }
+    }
+}
