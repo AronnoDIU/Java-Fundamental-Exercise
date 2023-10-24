eb986b56e90c6470b7a991c129f77cc55a8083c7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-24 14:41:09 +0600

    Trie Data Structure Implementation

--- src/CollectionsFramework/WordBreakTrieDS.java
+++ src/CollectionsFramework/WordBreakTrieDS.java
@@ -1,7 +1,16 @@
 package CollectionsFramework;
 
 /*Given an input string and a dictionary of words, find out if the input string can be
- * broken into a space-seperated sequence of dictionary words*/
+ * broken into a space-seperated sequence of dictionary words
+ *
+ * Steps to solve
+ * 1. Find all possible parts of String.
+ * 2. Using loop,
+ *      1st part, Trie Search;
+ *      2nd part, call recursive;
+ *          if, 1st & 2nd part return true, then it will return true;
+ * 3. Otherwise, it will return false;
+ * */
 public class WordBreakTrieDS {
     public static void main(String[] args) {
 
