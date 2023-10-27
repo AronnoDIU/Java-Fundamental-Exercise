5ac1f735bc5b9516ea8f646142b7eb1d3c9e00ed
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-27 22:41:36 +0600

    Graph Data Structure Implementation

--- src/CollectionsFramework/GraphDataStructure.java
+++ src/CollectionsFramework/GraphDataStructure.java
@@ -1,5 +1,7 @@
 package CollectionsFramework;
 
+import java.util.ArrayList;
+
 // Graph Data Structure Implementation ==> Network of Nodes.
 public class GraphDataStructure {
     static class Edge {
@@ -11,6 +13,27 @@ public class GraphDataStructure {
             this.dest = d;
             this.wt = w;
         }
+    }
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+        graph[5].add(new Edge(6, 5, 1));
     }
     public static void main(String[] args) {
 
