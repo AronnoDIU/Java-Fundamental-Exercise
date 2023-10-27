557798ece51a5e646b9bcf9e18732712c51429f8
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-27 22:43:59 +0600

    Graph Data Structure Implementation

--- src/CollectionsFramework/GraphDataStructure.java
+++ src/CollectionsFramework/GraphDataStructure.java
@@ -57,6 +57,16 @@ public class GraphDataStructure {
         System.out.println();
     }
     public static void main(String[] args) {
-
+        /*
+1 --- 3
+/ | \
+0 | 5 -- 6
+\ | /
+2 ---- 4
+*/
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        bfs(graph, V);
     }
 }
