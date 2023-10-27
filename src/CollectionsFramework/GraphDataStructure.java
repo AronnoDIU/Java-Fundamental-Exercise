801fce67d478b9bebdcf3805301b7893840a5d12
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-27 22:40:37 +0600

    Graph Data Structure Implementation

--- src/CollectionsFramework/GraphDataStructure.java
+++ src/CollectionsFramework/GraphDataStructure.java
@@ -2,6 +2,16 @@ package CollectionsFramework;
 
 // Graph Data Structure Implementation ==> Network of Nodes.
 public class GraphDataStructure {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
     public static void main(String[] args) {
 
     }
