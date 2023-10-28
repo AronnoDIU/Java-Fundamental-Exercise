0c859d660eb1279858f0d585d1b541b5c57997e3
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-28 17:06:59 +0600

    Graph Traversal using Depth-First Search

--- src/CollectionsFramework/GraphDataStructure/GraphTraversalDFS.java
+++ src/CollectionsFramework/GraphDataStructure/GraphTraversalDFS.java
@@ -2,42 +2,55 @@ package CollectionsFramework.GraphDataStructure;
 
 import java.util.ArrayList;
 
-public class GraphDataStructureDFS {
+// Graph Traversal using Depth-First Search
+public class GraphTraversalDFS {
     static class Edge {
-        int src;
-        int dest;
-        int wt;
+        int Source;
+        int Destination;
+        int Weight;
 
-        public Edge(int s, int d, int w) {
-            this.src = s;
-            this.dest = d;
-            this.wt = w;
+        public Edge(int source, int destination, int weight) {
+            Source = source;
+            Destination = destination;
+            Weight = weight;
         }
     }
 
-    static void createGraph(ArrayList<Edge>[] graph) {
+    static void createGraph(ArrayList<Edge>[] graph) {  // Adjacency List
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
+
+        // for 0 -vertex
         graph[0].add(new Edge(0, 1, 1));
         graph[0].add(new Edge(0, 2, 1));
+
+        // for 1 -vertex
         graph[1].add(new Edge(1, 0, 1));
         graph[1].add(new Edge(1, 3, 1));
+
+        // for 2 -vertex
         graph[2].add(new Edge(2, 0, 1));
         graph[2].add(new Edge(2, 4, 1));
+
+        // for 3 -vertex
         graph[3].add(new Edge(3, 1, 1));
         graph[3].add(new Edge(3, 4, 1));
         graph[3].add(new Edge(3, 5, 1));
+
+        // for 4 -vertex
         graph[4].add(new Edge(4, 2, 1));
         graph[4].add(new Edge(4, 3, 1));
         graph[4].add(new Edge(4, 5, 1));
+
+        // for 5 -vertex
         graph[5].add(new Edge(5, 3, 1));
         graph[5].add(new Edge(5, 4, 1));
         graph[5].add(new Edge(5, 6, 1));
         graph[5].add(new Edge(6, 5, 1));
     }
 
-    static void dfs(ArrayList<Edge>[] graph, int curr, boolean[] visited) {
+    static void DFS(ArrayList<Edge>[] graph, int curr, boolean[] visited) {
         if (visited[curr]) {
             return;
         }
@@ -45,7 +58,7 @@ public class GraphDataStructureDFS {
         visited[curr] = true;
         for (int i = 0; i < graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
-            dfs(graph, e.dest, visited);
+            DFS(graph, e.Destination, visited);
         }
     }
 
@@ -57,10 +70,12 @@ public class GraphDataStructureDFS {
             \ | /
             2 ---- 4
             */
-        int V = 7;
-        ArrayList<Edge>[] graph = new ArrayList[V];
+        int Vertex = 7;
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[Vertex];
         createGraph(graph);
-        dfs(graph, 0, new boolean[V]);
+        DFS(graph, 0, new boolean[Vertex]);
     }
 }
 
