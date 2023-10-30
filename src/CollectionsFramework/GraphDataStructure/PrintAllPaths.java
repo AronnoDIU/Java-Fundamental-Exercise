6bb33ed9644029b852109e1caa008bdb49535b18
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-10-30 21:12:26 +0600

    Print all paths

--- src/CollectionsFramework/GraphDataStructure/PrintAllPaths.java
+++ src/CollectionsFramework/GraphDataStructure/PrintAllPaths.java
@@ -18,21 +18,35 @@ public class PrintAllPaths {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
+
+        // for 0 -vertex
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
+
+        // for 1 -vertex
         graph[1].add(new Edge(1, 0));
         graph[1].add(new Edge(1, 3));
+
+        // for 2 -vertex
         graph[2].add(new Edge(2, 0));
         graph[2].add(new Edge(2, 4));
+
+        // for 3 -vertex
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 4));
         graph[3].add(new Edge(3, 5));
+
+        // for 4 -vertex
         graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
         graph[4].add(new Edge(4, 5));
+
+        // for 5 -vertex
         graph[5].add(new Edge(5, 3));
         graph[5].add(new Edge(5, 4));
         graph[5].add(new Edge(5, 6));
+
+        // for 6 -vertex
         graph[6].add(new Edge(6, 5));
     }
 
@@ -53,6 +67,24 @@ public class PrintAllPaths {
     }
 
     public static void main(String[] args) {
+        int Vertex = 7;
 
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[Vertex];
+        createGraph(graph);
+        int Source = 0;
+        int Destination = 5;
+        boolean[] visited = new boolean[Vertex];
+        visited[Source] = true;
+        printAllPaths(graph, Source, Destination, "" + Source, visited);
     }
 }
+
+/*Expected Output:
+
+0->1->3->4->5
+0->1->3->5
+0->2->4->3->5
+0->2->4->5
+
+* */
