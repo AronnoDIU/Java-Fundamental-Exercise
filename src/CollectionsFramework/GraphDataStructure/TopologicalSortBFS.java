0c74830bbe6fb094d96bc34f65e9aaf5a9a193ff
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-01 07:21:21 +0600

    Topological Sort using BFS

--- src/CollectionsFramework/GraphDataStructure/TopologicalSortBFS.java
+++ src/CollectionsFramework/GraphDataStructure/TopologicalSortBFS.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.GraphDataStructure;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * Topological Sort using BFS.
  * <p>
@@ -14,7 +17,75 @@ package CollectionsFramework.GraphDataStructure;
  * and at least one vertex with out-degree 0.
  */
 public class TopologicalSortBFS {
-    public static void main(String[] args) {
+    static class Edge {
+        int Source;
+        int Destination;
 
+        public Edge(int source, int destination) {
+            Source = source;
+            Destination = destination;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // for 2 -vertex
+        graph[2].add(new Edge(2, 3));
+
+        // for 3 -vertex
+        graph[3].add(new Edge(3, 1));
+
+        // for 4 -vertex
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        // for 5 -vertex
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
+    static void topologicalSortUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis,
+                                    Stack<Integer> s) {
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.Destination]) {
+                topologicalSortUtil(graph, e.Destination, vis, s);
+            }
+        }
+        s.push(curr);
+    }
+
+    //O(V+E)
+    static void topologicalSort(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                topologicalSortUtil(graph, i, vis, s);
+            }
+        }
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+             5              4
+             |  \         / |
+             |   \      /   |
+             |    > 0 <     |
+             |              |
+             >              <
+             2---->3------->1
+         */
+        int Vertex = 6;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[Vertex]; // Adjacency List.
+        createGraph(graph); // Calling the function to create the graph.
     }
 }
