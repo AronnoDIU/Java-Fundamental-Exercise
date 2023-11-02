20109632a10c0d6af9238811fe61e4d27aab94f4
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-02 11:39:45 +0600

    Dijkstra's Algorithm

---
+++ src/CollectionsFramework/GraphDataStructure/DijkstrasAlgorithm.java
@@ -0,0 +1,18 @@
+package CollectionsFramework.GraphDataStructure;
+
+/** Dijkstra's Algorithm => is an algorithm for finding the shortest paths between
+ nodes in a weighted graph, where the weight of each edge is non-negative.*/
+public class DijkstrasAlgorithm {
+    public static void main(String[] args) {
+        /*
+             5              4
+             |  \         / |
+             |   \      /   |
+             |    > 0 <     |
+             |      |       |
+             >      |<      <
+             2---->3------->1
+         */
+        int Vertex = 6;
+    }
+}
