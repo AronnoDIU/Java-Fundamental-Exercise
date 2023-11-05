4c1aec116fb3d4ef6d4e04e5aee107e1dc3f2ad7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-05 20:32:54 +0600

    Bellman Ford Algorithm (Shortest Distance)

--- src/CollectionsFramework/GraphDataStructure/BellmanFordAlgorithm.java
+++ src/CollectionsFramework/GraphDataStructure/BellmanFordAlgorithm.java
@@ -8,14 +8,16 @@ public class BellmanFordAlgorithm {
         int src;
         int dest;
         int wt;
+
         public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
             this.wt = w;
         }
     }
-    static void createGraph(ArrayList<Edge> graph[]) {
-        for(int i=0; i<graph.length; i++) {
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1, 2));
@@ -25,45 +27,46 @@ public class BellmanFordAlgorithm {
         graph[3].add(new Edge(3, 4, 4));
         graph[4].add(new Edge(4, 1, -1));
     }
-    public static void bellmanFord(ArrayList<Edge> graph[], int src) {
-        int dist[] = new int[graph.length];
-        for(int i=0; i<dist.length; i++) {
-            if(i != src)
+
+    static void bellmanFord(ArrayList<Edge>[] graph, int src) {
+        int[] dist = new int[graph.length];
+        for (int i = 0; i < dist.length; i++) {
+            if (i != src)
                 dist[i] = Integer.MAX_VALUE;
         }
 //O(V)
-        for(int i=0; i<graph.length-1; i++) {
+        for (int i = 0; i < graph.length - 1; i++) {
 //edges - O(E)
-            for(int j=0; j<graph.length; j++) {
-                for(int k=0; k<graph[j].size(); k++) {
+            for (int j = 0; j < graph.length; j++) {
+                for (int k = 0; k < graph[j].size(); k++) {
                     Edge e = graph[j].get(k);
                     int u = e.src;
                     int v = e.dest;
                     int wt = e.wt;
-                    if(dist[u] != Integer.MAX_VALUE && dist[u]+wt < dist[v]) {
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
                         dist[v] = dist[u] + wt;
                     }
                 }
             }
         }
 //Detecting Negative Weight Cycle
-        for(int j=0; j<graph.length; j++) {
-            for(int k=0; k<graph[j].size(); k++) {
-                Edge e = graph[j].get(k);
+        for (ArrayList<Edge> edges : graph) {
+            for (Edge e : edges) {
                 int u = e.src;
                 int v = e.dest;
                 int wt = e.wt;
-                if(dist[u] != Integer.MAX_VALUE && dist[u]+wt < dist[v]) {
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
                     System.out.println("negative weight cycle exists");
                     break;
                 }
             }
         }
-        for(int i=0; i<dist.length; i++) {
-            System.out.print(dist[i]+" ");
+        for (int j : dist) {
+            System.out.print(j + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
 
     }
