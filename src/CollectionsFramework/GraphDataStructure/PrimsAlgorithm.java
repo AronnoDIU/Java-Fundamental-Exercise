fb10025d388db603764520e67991c2e793abf6f5
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-06 15:17:38 +0600

    Prim’s Algorithm (Minimum Spanning Tree)

---
+++ src/CollectionsFramework/GraphDataStructure/PrimsAlgorithm.java
@@ -0,0 +1,8 @@
+package CollectionsFramework.GraphDataStructure;
+
+// Prim’s Algorithm (Minimum Spanning Tree)
+public class PrimsAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
