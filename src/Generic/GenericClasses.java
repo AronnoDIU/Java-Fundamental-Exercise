ad878de5a9fe127a544bfc4ccaa5abbcb76b676d
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-09 23:25:23 +0600

    User defined Generic classes

--- src/Generic/GenericClasses.java
+++ src/Generic/GenericClasses.java
@@ -4,16 +4,45 @@ package Generic;
 public class GenericClasses {
     public static void main(String[] args) {
 
+        GenericTest<Integer> gt = new GenericTest<>(10);
+        System.out.println(gt.getObj());
+
+        GenericTest<String> gt1 = new GenericTest<>("Yeasir");
+        System.out.println(gt1.getObj());
+
+        GenericTest<Boolean> gt2 = new GenericTest<>(true);
+        System.out.println(gt2.getObj());
+
+        GenericTest<Double> gt3 = new GenericTest<>(10.5);
+        System.out.println(gt3.getObj());
+
+        GenericTest<Float> gt4 = new GenericTest<>(10.5f);
+        System.out.println(gt4.getObj());
+
+        GenericTest<Character> gt5 = new GenericTest<>('A');
+        System.out.println(gt5.getObj());
     }
 }
 
-class GenericTest<T>{
-    T obj;
-    GenericTest(T obj){
+class GenericTest<T> {
+    T obj; // An object of type T is created.
+
+    GenericTest(T obj) {
         this.obj = obj;
     }
 
-    public T getObj(){
+    public T getObj() {
         return obj;
     }
 }
+
+/*Expected Output:
+
+10
+Yeasir
+true
+10.5
+10.5
+A
+
+* */
