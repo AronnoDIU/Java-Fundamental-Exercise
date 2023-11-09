2bed15ee8445175d05685538ca3b79b15e244639
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-09 23:31:12 +0600

    User defined Generic classes

--- src/Generic/GenericClasses.java
+++ src/Generic/GenericClasses.java
@@ -5,22 +5,22 @@ public class GenericClasses {
     public static void main(String[] args) {
 
         GenericTest<Integer> genericTest1 = new GenericTest<>(10);
-        System.out.println(genericTest1.getObj());
+        System.out.println("Integer Type : " + genericTest1.getObj());
 
-        GenericTest<String> genericTest2 = new GenericTest<>("Yeasir");
-        System.out.println(genericTest2.getObj());
+        GenericTest<String> genericTest2 = new GenericTest<>("Yeasir Arafat Aronno");
+        System.out.println("String Type : " + genericTest2.getObj());
 
         GenericTest<Boolean> genericTest3 = new GenericTest<>(true);
-        System.out.println(genericTest3.getObj());
+        System.out.println("Boolean Type : " + genericTest3.getObj());
 
         GenericTest<Double> genericTest4 = new GenericTest<>(10.5);
-        System.out.println(genericTest4.getObj());
+        System.out.println("Double Type : " + genericTest4.getObj());
 
         GenericTest<Float> genericTest5 = new GenericTest<>(10.5f);
-        System.out.println(genericTest5.getObj());
+        System.out.println("Float Type : " + genericTest5.getObj());
 
         GenericTest<Character> genericTest6 = new GenericTest<>('A');
-        System.out.println(genericTest6.getObj());
+        System.out.println("Character Type : " + genericTest6.getObj());
     }
 }
 
@@ -38,11 +38,11 @@ class GenericTest<T> {
 
 /*Expected Output:
 
-10
-Yeasir
-true
-10.5
-10.5
-A
+Integer Type : 10
+String Type : Yeasir Arafat Aronno
+Boolean Type : true
+Double Type : 10.5
+Float Type : 10.5
+Character Type : A
 
 * */
