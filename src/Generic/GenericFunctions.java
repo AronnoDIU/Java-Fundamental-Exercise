e1d8b3f2199a80fbb2325370e4ff9039dc496b7e
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-10 10:41:19 +0600

    Generic Functions

--- src/Generic/GenericFunctions.java
+++ src/Generic/GenericFunctions.java
@@ -1,16 +1,40 @@
 package Generic;
 
+import java.io.Serializable;
+
 // Generic functions: That can be called with different types of arguments
 // based on the type of arguments passed to the generic method.
 public class GenericFunctions {
     public static void main(String[] args) {
         System.out.printf("Max of %d, %d and %d is %d" +
                 "\n\n", 1, 2, 3, GFunctions.max(1, 2, 3));
+
+        // We can call this method with any type of object
+
+        Integer max = GFunctions.max(1, 2, 3);
+
+        System.out.printf("Max of %d, %d and %d is %d" +
+                "\n\n", 1, 2, 3, max);
+
+        GFunctions.exampleMethod(1);
+        GFunctions.exampleMethod("Yeasir Arafat Aronno");
+        GFunctions.exampleMethod(1.1f);
+        GFunctions.exampleMethod(true);
     }
 }
 
 class GFunctions {
-    public static <T> T max(T x, T y, T z) {
+    public static <T extends Comparable<T> & Serializable> void exampleMethod(T obj) {
+
+        System.out.println("Max of " + obj + " is " + GFunctions.max(obj, obj, obj));
+
+        // We can call this method with any type of object
+        T max = GFunctions.max(obj, obj, obj);
+
+        System.out.println("Max of " + obj + " is " + max);
+    }
+
+    public static <T extends Comparable<T>> T max(T x, T y, T z) {
         T max = x; // assume x is initially the largest
 
         if (y.compareTo(max) > 0) {
@@ -25,7 +49,17 @@ class GFunctions {
 
 /*Expected Output:
 
-Articulation Points : 0
-Articulation Points : 3
+Max of 1, 2 and 3 is 3
+
+Max of 1, 2 and 3 is 3
+
+Max of 1 is 1
+Max of 1 is 1
+Max of Yeasir Arafat Aronno is Yeasir Arafat Aronno
+Max of Yeasir Arafat Aronno is Yeasir Arafat Aronno
+Max of 1.1 is 1.1
+Max of 1.1 is 1.1
+Max of true is true
+Max of true is true
 
 * */
