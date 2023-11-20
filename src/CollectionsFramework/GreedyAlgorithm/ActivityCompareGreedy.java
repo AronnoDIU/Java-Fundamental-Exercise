b20f43a1c8a34f1a176d88a8eec4df827f3ba00a
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-20 21:58:22 +0600

    Activity Compare

---
+++ src/CollectionsFramework/GreedyAlgorithm/ActivityCompareGreedy.java
@@ -0,0 +1,9 @@
+package CollectionsFramework.GreedyAlgorithm;
+
+public class ActivityCompareGreedy {
+    public static void main(String[] args) {
+        int[] s = {1, 3, 0, 5, 8, 5};
+        int[] f = {2, 4, 6, 7, 9, 9};
+        int n = s.length;
+    }
+}
