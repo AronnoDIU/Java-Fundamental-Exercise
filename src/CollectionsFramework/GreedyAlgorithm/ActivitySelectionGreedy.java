d3688d221ef576f2dfbbebfa81e239f1fe2ccafe
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-20 21:55:50 +0600

    Activity Selection

--- src/CollectionsFramework/GreedyAlgorithm/ActivitySelectionGreedy.java
+++ src/CollectionsFramework/GreedyAlgorithm/ActivitySelectionGreedy.java
@@ -6,7 +6,7 @@ public class ActivitySelectionGreedy {
     public static void printMaxActivities(int[] s, int[] f, int n) {
         int i, j;
 
-        System.out.println("Following activities are selected");
+        System.out.println("The Following activities are selected");
 
         // The first activity always gets selected
         i = 0;
@@ -14,9 +14,8 @@ public class ActivitySelectionGreedy {
 
         // Consider the rest of the activities
         for (j = 1; j < n; j++) {
-            // If this activity has start time greater than
-            // or equal to the finish time of previously
-            // selected activity, then select it
+            // If this activity has start time greater than or equal to the finish time of
+            // previously selected activity, then select it
             if (s[j] >= f[i]) {
                 System.out.print(j + " ");
                 i = j;
@@ -25,6 +24,18 @@ public class ActivitySelectionGreedy {
     }
 
     public static void main(String[] args) {
-        String[] wordsIS = {"the", "a", "there", "their", "any", "thee"};
+        int[] s = {1, 3, 0, 5, 8, 5};
+        int[] f = {2, 4, 6, 7, 9, 9};
+        int n = s.length;
+
+        // Function call
+        printMaxActivities(s, f, n);
     }
 }
+
+/*Expected Output:
+
+The Following activities are selected
+0 1 3 4
+
+ */
