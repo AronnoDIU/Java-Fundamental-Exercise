437ccd3b04e7cb4e11cf185a1707f4d220be4f72
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-20 22:02:35 +0600

    Activity Compare

--- src/CollectionsFramework/GreedyAlgorithm/ActivityCompareGreedy.java
+++ src/CollectionsFramework/GreedyAlgorithm/ActivityCompareGreedy.java
@@ -4,6 +4,35 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class ActivityCompareGreedy {
+
+    // Returns count of the maximum set of activities that
+    // can
+    // be done by a single person, one at a time.
+    static void printMaxActivities(Activity[] arr, int n) {
+        // Sort jobs according to finish time
+//        Compare obj = new Compare();
+        Compare.compare(arr, n);
+        System.out.println(
+                "Following activities are selected :");
+
+        // The first activity always gets selected
+        int i = 0;
+        System.out.print("(" + arr[i].start + ", "
+                + arr[i].finish + ")");
+
+        // Consider the rest of the activities
+        for (int j = 1; j < n; j++) {
+
+            // If this activity has start time greater than
+            // or equal to the finish time of previously
+            // selected activity, then select it
+            if (arr[j].start >= arr[i].finish) {
+                System.out.print(", (" + arr[j].start + ", "
+                        + arr[j].finish + ")");
+                i = j;
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] s = {1, 3, 0, 5, 8, 5};
         int[] f = {2, 4, 6, 7, 9, 9};
@@ -36,3 +65,10 @@ class Compare {
         });
     }
 }
+
+/*Expected Output:
+
+The Following activities are selected
+0 1 3 4
+
+ */
