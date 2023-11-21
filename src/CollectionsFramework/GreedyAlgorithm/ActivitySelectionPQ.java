ba49b5b1f715487ba70d2156770543fd9784db80
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-21 17:05:35 +0600

    Activity Selection using Priority-Queue

--- src/CollectionsFramework/GreedyAlgorithm/ActivitySelectionPQ.java
+++ src/CollectionsFramework/GreedyAlgorithm/ActivitySelectionPQ.java
@@ -1,6 +1,7 @@
 package CollectionsFramework.GreedyAlgorithm;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 // Activity Selection using Priority-Queue
@@ -17,7 +18,7 @@ public class ActivitySelectionPQ {
         }
     }
 
-    static void SelectActivities(int s[], int f[]) {
+    static void SelectActivities(int[] s, int[] f) {
 
         // Vector to store results.
         ArrayList<Pair> ans = new ArrayList<>();
@@ -25,7 +26,7 @@ public class ActivitySelectionPQ {
         // Minimum Priority Queue to sort activities in
         // ascending order of finishing time (f[i]).
         PriorityQueue<Pair> p = new PriorityQueue<>(
-                (p1, p2) -> p1.first - p2.first);
+                Comparator.comparingInt(p2 -> p2.first));
 
         for (int i = 0; i < s.length; i++) {
             // Pushing elements in the priority queue where the
@@ -34,6 +35,7 @@ public class ActivitySelectionPQ {
         }
 
         Pair it = p.poll();
+        assert it != null;
         int start = it.second;
         int end = it.first;
         ans.add(new Pair(start, end));
@@ -47,7 +49,7 @@ public class ActivitySelectionPQ {
             }
         }
         System.out.println(
-                "Following Activities should be selected. \n");
+                "The Following Activities should be selected. \n");
 
         for (Pair itr : ans) {
             System.out.println(
@@ -59,8 +61,19 @@ public class ActivitySelectionPQ {
     public static void main(String[] args) {
         int[] s = {1, 3, 0, 5, 8, 5};
         int[] f = {2, 4, 6, 7, 9, 9};
-        int n = s.length;
 
-
+        // Function call
+        SelectActivities(s, f);
     }
 }
+
+/*Expected Output:
+
+The Following Activities should be selected.
+
+Activity started at 1 and ends at 2
+Activity started at 3 and ends at 4
+Activity started at 5 and ends at 7
+Activity started at 8 and ends at 9
+
+ */
