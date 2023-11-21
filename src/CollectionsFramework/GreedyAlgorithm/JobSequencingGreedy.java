51a09d62077e7cbe49d27ca5c43f226a6e504cf5
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-21 17:07:50 +0600

    Job Sequencing

---
+++ src/CollectionsFramework/GreedyAlgorithm/JobSequencingGreedy.java
@@ -0,0 +1,11 @@
+package CollectionsFramework.GreedyAlgorithm;
+
+public class JobSequencingGreedy {
+    public static void main(String[] args) {
+        int[] profit = {20, 15, 10, 5, 1};
+        int[] deadline = {2, 2, 1, 3, 3};
+        int n = profit.length;
+
+
+    }
+}
