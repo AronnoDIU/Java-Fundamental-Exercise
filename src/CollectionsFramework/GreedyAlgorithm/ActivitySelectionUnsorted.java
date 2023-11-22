bc1ae637b03bb9e9074adb5916cf5acb142f7525
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-23 05:12:08 +0600

    Activity Selection Unsorted

---
+++ src/CollectionsFramework/GreedyAlgorithm/ActivitySelectionUnsorted.java
@@ -0,0 +1,37 @@
+package CollectionsFramework.GreedyAlgorithm;
+
+/*
+ A job has a start time, finish time and profit.
+
+    2D Array: Workings of the Greedy Algorithm
+    1. Sort the jobs according to their finishing time.
+
+    2D Array:           0    1    2
+                 A0   |----|----|----|
+                 A1   |----|----|----|
+                 A2   |----|----|----|
+                 A3   |----|----|----|
+                 A4   |----|----|----|
+                 A5   |----|----|----|
+
+        Here, A0, A1, A2, A3, A4, A5 Store the activities.
+              => 0 will Store the Index.
+              =>1, 2 Store the time slots.{ 1 for start & 2 for finish}
+
+
+*/
+public class ActivitySelectionUnsorted {
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+
+        // End time basis sorted array
+        int[][] maxActivities = new int[start.length][3]; // initialize maxActivities = 0
+
+        for (int index = 0; index < start.length; index++) {
+            maxActivities[index][0] = index;
+            maxActivities[index][1] = start[index];
+            maxActivities[index][2] = end[index];
+        }
+    }
+}
