56281a82940da47d8a35f7540f784a6c83444260
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-30 08:32:52 +0600

    Water Jug problem using BFS

---
+++ src/CollectionsFramework/GreedyAlgorithm/WaterJugBFS.java
@@ -0,0 +1,11 @@
+package CollectionsFramework.GreedyAlgorithm;
+
+public class WaterJugBFS {
+    public static void main(String[] args) {
+        int jug1 = 4;
+        int jug2 = 3;
+        int target = 2;
+
+        System.out.println("Steps: " + waterJug(jug1, jug2, target));
+    }
+}
