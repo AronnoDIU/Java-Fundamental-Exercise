c632aed61a02e926fbf0c6808fc4ccda6e4aa39b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-08 05:44:45 +0600

    Fitting Shelves

--- src/CollectionsFramework/GreedyAlgorithm/FittingShelves.java
+++ src/CollectionsFramework/GreedyAlgorithm/FittingShelves.java
@@ -2,6 +2,7 @@ package CollectionsFramework.GreedyAlgorithm;
 
 // Java program to count all rotation divisible by 4.
 public class FittingShelves {
+    static void minSpacePreferLarge(int wall, int m, int n){
     // For simplicity, Assuming m is always smaller than n
         // initializing output variables
         int num_m = 0, num_n = 0, min_empty = wall;
@@ -9,6 +10,7 @@ public class FittingShelves {
         // p and q are no of shelves of length m and n
         // rem is the empty space
         int p = wall/m, q = 0, rem=wall%m;
+    
     
     public static void main(String[] args) {
         String s = "[]][][";
