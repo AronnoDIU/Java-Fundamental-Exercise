9dc52ac1f7e85bce2a18bb690df742d9e2d76723
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-07 22:23:26 +0600

    Update AssignMiceHoles.java

--- src/CollectionsFramework/GreedyAlgorithm/AssignMiceHoles.java
+++ src/CollectionsFramework/GreedyAlgorithm/AssignMiceHoles.java
@@ -4,6 +4,28 @@ package CollectionsFramework.GreedyAlgorithm;
 // all mice in all holes.
 import java.util.* ;
 public class AssignMiceHoles {
+    // Returns minimum time required to place mice
+    // in holes.
+    public int assignHole(ArrayList<Integer> mice,
+                         ArrayList<Integer> holes)
+    {
+        if (mice.size() != holes.size())
+           return -1;
+ 
+        /* Sort the lists */
+        Collections.sort(mice);
+        Collections.sort(holes);
+ 
+        int size = mice.size();
+ 
+        /* finding max difference between ith mice and hole */
+        int max = 0;
+        for (int i=0; i<size; i++)
+            if (max < Math.abs(mice.get(i)-holes.get(i)))
+                max = Math.abs(mice.get(i)-holes.get(i));
+ 
+        return Math.abs(max);
+    }
     public static void main(String[] args) {
         String s = "[]][][";
     }
