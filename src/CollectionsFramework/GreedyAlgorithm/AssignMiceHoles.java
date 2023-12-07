1080f34adad8138bbe8f982a653bbb9571517391
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-07 17:44:25 +0600

    Update AssignMiceHoles.java

--- src/CollectionsFramework/GreedyAlgorithm/AssignMiceHoles.java
+++ src/CollectionsFramework/GreedyAlgorithm/AssignMiceHoles.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.GreedyAlgorithm;
 
+// Java program to find the minimum time to place
+// all mice in all holes.
+import java.util.* ;
 public class AssignMiceHoles {
     public static void main(String[] args) {
         String s = "[]][][";
