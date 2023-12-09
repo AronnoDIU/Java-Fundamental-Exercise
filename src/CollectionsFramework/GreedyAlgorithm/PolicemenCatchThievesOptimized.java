791e85cac662ae0ce7adc8e9776822b2a10feb97
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-10 04:48:04 +0600

    PolicemenCatchThievesOptimized

--- src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThievesOptimized.java
+++ src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThievesOptimized.java
@@ -69,6 +69,8 @@ public class PolicemenCatchThievesOptimized {
                     pol++;
             }
         }
+        return res;
+    }
     public static void main(String[] args) {
         String s = "[]][][";
     }
