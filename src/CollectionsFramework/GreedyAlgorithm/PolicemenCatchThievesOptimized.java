b4e7c2c65cfe41024a1aad8dfa07d66645a3fa0d
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-09 06:39:04 +0600

    Policemen Catch Thieves Optimized

--- src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThievesOptimized.java
+++ src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThievesOptimized.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.GreedyAlgorithm;
 
+// Java program to find maximum number of thieves caught
+import java.io.*;
+import java.util.*;
 public class PolicemenCatchThievesOptimized {
     public static void main(String[] args) {
         String s = "[]][][";
