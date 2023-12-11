8aed47f07fb02d29d25b6d25119d29a476993cec
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-11 09:52:34 +0600

    PolicemenCatchThieves

--- src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThieves.java
+++ src/CollectionsFramework/GreedyAlgorithm/PolicemenCatchThieves.java
@@ -9,6 +9,9 @@ public class PolicemenCatchThieves {
     // Returns maximum number of thieves
     // that can be caught.
     static int policeThief(char arr[], int n, int k) {
+        int res = 0;
+        ArrayList<Integer> thi = new ArrayList<Integer>();
+        ArrayList<Integer> pol = new ArrayList<Integer>();
         
     
     public static void main(String[] args) {
