b96b8dffd780870031816ba64142e4b8dd79da69
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-12 09:16:11 +0600

    EgyptianFractionOptimized

--- src/CollectionsFramework/GreedyAlgorithm/EgyptianFractionOptimized.java
+++ src/CollectionsFramework/GreedyAlgorithm/EgyptianFractionOptimized.java
@@ -2,6 +2,12 @@ package CollectionsFramework.GreedyAlgorithm;
 import java.util.*;
 
 public class EgyptianFractionOptimized {
+    static Vector<Integer>
+    getEgyptianFractionUtil(int numerator, int denominator,
+                            Vector<Integer> listOfDenoms) {
+        if (numerator == 0)
+            return listOfDenoms;
+    
     public static void main(String[] args) {
         String s = "[]][][";
     }
