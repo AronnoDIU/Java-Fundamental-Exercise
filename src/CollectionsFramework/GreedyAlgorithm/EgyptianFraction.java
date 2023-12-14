c41427a895a6946104b0ad03d861205fa5048640
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-14 06:48:21 +0600

    Update EgyptianFraction.java

--- src/CollectionsFramework/GreedyAlgorithm/EgyptianFraction.java
+++ src/CollectionsFramework/GreedyAlgorithm/EgyptianFraction.java
@@ -3,6 +3,13 @@ package CollectionsFramework.GreedyAlgorithm;
 // Java program to print a fraction
 // in Egyptian Form using Greedy Algorithm
 public class EgyptianFraction {
+    static void printEgyptian(int nr, int dr) {
+        // If either numerator or
+        // denominator is 0
+        if (dr == 0 || nr == 0) {
+            return;
+        }
+    
     public static void main(String[] args) {
         String s = "[]][][";
     }
