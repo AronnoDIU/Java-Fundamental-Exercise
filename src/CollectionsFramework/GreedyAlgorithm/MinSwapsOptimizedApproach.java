95a32b6d157bdeeb656bc61c0f686fcf2fca7a60
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-17 08:02:25 +0600

    Update MinSwapsOptimizedApproach.java

--- src/CollectionsFramework/GreedyAlgorithm/MinSwapsOptimizedApproach.java
+++ src/CollectionsFramework/GreedyAlgorithm/MinSwapsOptimizedApproach.java
@@ -1,5 +1,8 @@
 package CollectionsFramework.GreedyAlgorithm;
 
+// Java program to count swaps 
+// required to balance string
+import java.util.*;
 public class MinSwapsOptimizedApproach {
     public static void main(String[] args) {
         String s = "[]][][";
