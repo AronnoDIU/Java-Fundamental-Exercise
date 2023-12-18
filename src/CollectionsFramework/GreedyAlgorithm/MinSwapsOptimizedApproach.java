c8ad4a64f90fe57a3825e3417f9e82034a9e065b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-18 18:11:37 +0600

    Min Swaps Optimized Approach

--- src/CollectionsFramework/GreedyAlgorithm/MinSwapsOptimizedApproach.java
+++ src/CollectionsFramework/GreedyAlgorithm/MinSwapsOptimizedApproach.java
@@ -2,66 +2,64 @@ package CollectionsFramework.GreedyAlgorithm;
 
 // Java program to count swaps 
 // required to balance string
+
 import java.util.*;
+
 public class MinSwapsOptimizedApproach {
     // Function to calculate swaps required
-public static long swapCount(String s) {
-     
-    // Keep track of '['
-    Vector<Integer> pos = new Vector<Integer>(); 
-    for(int i = 0; i < s.length(); ++i)
-        if (s.charAt(i) == '[')
-            pos.add(i);
+    public static long swapCount(String s) {
 
-    // To count the number of encountered '['
-    int count = 0; 
-     
-    // To track the position of next '[' in pos
-    int p = 0;  
-     
-    // To store result
-    long sum = 0;
+        // Keep track of '['
+        Vector<Integer> pos = new Vector<>();
+        for (int i = 0; i < s.length(); ++i)
+            if (s.charAt(i) == '[')
+                pos.add(i);
 
-    char[] S = s.toCharArray(); 
-     
-    for(int i = 0; i < s.length(); ++i)
-    {
-         
-        // Increment count and move p 
-        // to next position
-        if (S[i] == '[')
-        {
-            ++count;
-            ++p;
+        // To count the number of encountered '['
+        int count = 0;
+
+        // To track the position of next '[' in pos
+        int p = 0;
+
+        // To store result
+        long sum = 0;
+
+        char[] S = s.toCharArray();
+
+        for (int i = 0; i < s.length(); ++i) {
+
+            // Increment count and move p
+            // to next position
+            if (S[i] == '[') {
+                ++count;
+                ++p;
+            } else if (S[i] == ']')
+                --count;
+
+            // We have encountered an
+            // unbalanced part of string
+            if (count < 0) {
+
+                // Increment sum by number of
+                // swaps required i.e., position
+                // of next '[' - current position
+                sum += pos.get(p) - i;
+                char temp = S[i];
+                S[i] = S[pos.get(p)];
+                S[pos.get(p)] = temp;
+                ++p;
+                // Reset count to 1
+                count = 1;
+            }
         }
-        else if (S[i] == ']')
-            --count;
-
-        // We have encountered an 
-        // unbalanced part of string
-        if (count < 0)
-        {
-             
-            // Increment sum by number of 
-            // swaps required i.e., position
-            // of next '[' - current position
-            sum += pos.get(p) - i;
-            char temp = S[i];
-            S[i] = S[pos.get(p)];
-            S[pos.get(p)] = temp;
-            ++p;
-            // Reset count to 1
-            count = 1;
-        }
+        return sum;
     }
-    return sum;
-}
-    
+
     public static void main(String[] args) {
         String s = "[]][][";
-    System.out.println(swapCount(s));
-  
-    s = "[[][]]";
-    System.out.println(swapCount(s));
+        System.out.println(swapCount(s));
+
+        s = "[[][]]";
+        System.out.println(swapCount(s));
     }
 }
