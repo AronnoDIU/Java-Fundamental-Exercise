51bf64027b2a9156d10fd4a336c7c52068076c83
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-22 23:47:19 +0600

    Water Jug BFS

--- src/CollectionsFramework/GreedyAlgorithm/WaterJugBFS.java
+++ src/CollectionsFramework/GreedyAlgorithm/WaterJugBFS.java
@@ -7,22 +7,22 @@ import java.util.Queue;
 
 public class WaterJugBFS {
     static class Pair {
-        int j1, j2;
+        int jug1, jug2;
         List<Pair> path;
 
-        Pair(int j1, int j2) {
-            this.j1 = j1;
-            this.j2 = j2;
+        Pair(int jug1, int jug2) {
+            this.jug1 = jug1;
+            this.jug2 = jug2;
             path = new ArrayList<>();
         }
 
-        Pair(int j1, int j2, List<Pair> _path) {
-            this.j1 = j1;
-            this.j2 = j2;
+        Pair(int jug1, int jug2, List<Pair> _path) {
+            this.jug1 = jug1;
+            this.jug2 = jug2;
 
             path = new ArrayList<>();
             path.addAll(_path);
-            path.add(new Pair(this.j1, this.j2));
+            path.add(new Pair(this.jug1, this.jug2));
         }
     }
 
@@ -50,31 +50,31 @@ public class WaterJugBFS {
             Pair curr = queue.poll();
 
             // Skip already visited states and overflowing water states
-            if (curr.j1 > jug1 || curr.j2 > jug2
-                    || visited[curr.j1][curr.j2])
+            if (curr.jug1 > jug1 || curr.jug2 > jug2
+                    || visited[curr.jug1][curr.jug2])
                 continue;
             // mark current jugs state as visited
-            visited[curr.j1][curr.j2] = true;
+            visited[curr.jug1][curr.jug2] = true;
 
             // Check if the current state has already reached
             // the target amount of water or not
-            if (curr.j1 == target || curr.j2 == target) {
-                if (curr.j1 == target) {
+            if (curr.jug1 == target || curr.jug2 == target) {
+                if (curr.jug1 == target) {
                     // If in our current state, jug1 holds the required amount of water,
                     // then we empty the jug2 and push it into our path.
-                    curr.path.add(new Pair(curr.j1, 0));
+                    curr.path.add(new Pair(curr.jug1, 0));
                 } else {
                     // else, If in our current state, jug2 holds the required amount of water,
                     // then we empty the jug1 and push it into our path.
-                    curr.path.add(new Pair(0, curr.j2));
+                    curr.path.add(new Pair(0, curr.jug2));
                 }
                 int n = curr.path.size();
                 System.out.println(
                         "Path of states of jugs followed is :");
                 for (int i = 0; i < n; i++)
                     System.out.println(
-                            curr.path.get(i).j1 + " , "
-                                    + curr.path.get(i).j2);
+                            curr.path.get(i).jug1 + " , "
+                                    + curr.path.get(i).jug2);
                 return;
             }
 
@@ -91,30 +91,30 @@ public class WaterJugBFS {
             queue.offer(new Pair(0, jug2, curr.path));
 
             // II. Fill the jug and let the other remain untouched
-            queue.offer(new Pair(jug1, curr.j2, curr.path));
-            queue.offer(new Pair(curr.j1, jug2, curr.path));
+            queue.offer(new Pair(jug1, curr.jug2, curr.path));
+            queue.offer(new Pair(curr.jug1, jug2, curr.path));
 
             // III. Empty the jug and let the other remain untouched
-            queue.offer(new Pair(0, curr.j2, curr.path));
-            queue.offer(new Pair(curr.j1, 0, curr.path));
+            queue.offer(new Pair(0, curr.jug2, curr.path));
+            queue.offer(new Pair(curr.jug1, 0, curr.path));
 
             // IV. Transfer water from one to another until
             // one jug becomes empty or until one jug
             // becomes full in this process
 
             // Transferring water form jug1 to jug2
-            int emptyJug = jug2 - curr.j2;
+            int emptyJug = jug2 - curr.jug2;
             int amountTransferred
-                    = Math.min(curr.j1, emptyJug);
-            int j2 = curr.j2 + amountTransferred;
-            int j1 = curr.j1 - amountTransferred;
+                    = Math.min(curr.jug1, emptyJug);
+            int j2 = curr.jug2 + amountTransferred;
+            int j1 = curr.jug1 - amountTransferred;
             queue.offer(new Pair(j1, j2, curr.path));
 
             // Transferring water form jug2 to jug1
-            emptyJug = jug1 - curr.j1;
-            amountTransferred = Math.min(curr.j2, emptyJug);
-            j2 = curr.j2 - amountTransferred;
-            j1 = curr.j1 + amountTransferred;
+            emptyJug = jug1 - curr.jug1;
+            amountTransferred = Math.min(curr.jug2, emptyJug);
+            j2 = curr.jug2 - amountTransferred;
+            j1 = curr.jug1 + amountTransferred;
             queue.offer(new Pair(j1, j2, curr.path));
         }
 
