aef0b43850f165bf89a3e94f2f1cc24912d3435e
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-01-07 20:53:36 +0600

    ComplexNumberSystem

---
+++ src/NumberSystemsData/ComplexNumberSystem.java
@@ -0,0 +1,50 @@
+package NumberSystemsData;
+
+public class ComplexNumberSystem {
+
+    public record ComplexNumber(double real, double imaginary) {
+
+        public ComplexNumber add(ComplexNumber other) {
+                return new ComplexNumber(this.real + other.real, this.imaginary + other.imaginary);
+            }
+
+            public ComplexNumber subtract(ComplexNumber other) {
+                return new ComplexNumber(this.real - other.real, this.imaginary - other.imaginary);
+            }
+
+            public ComplexNumber multiply(ComplexNumber other) {
+                double realPart = this.real * other.real - this.imaginary * other.imaginary;
+                double imaginaryPart = this.real * other.imaginary + this.imaginary * other.real;
+                return new ComplexNumber(realPart, imaginaryPart);
+            }
+
+            public ComplexNumber divide(ComplexNumber other) {
+                double denominator = other.real * other.real + other.imaginary * other.imaginary;
+                double realPart = (this.real * other.real + this.imaginary * other.imaginary) / denominator;
+                double imaginaryPart = (this.imaginary * other.real - this.real * other.imaginary) / denominator;
+                return new ComplexNumber(realPart, imaginaryPart);
+            }
+
+            @Override
+            public String toString() {
+                return real + " + " + imaginary + "i";
+            }
+        }
+
+    public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(4, 5);
+        ComplexNumber num2 = new ComplexNumber(2, 3);
+
+        ComplexNumber sum = num1.add(num2);
+        ComplexNumber difference = num1.subtract(num2);
+        ComplexNumber product = num1.multiply(num2);
+        ComplexNumber quotient = num1.divide(num2);
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
