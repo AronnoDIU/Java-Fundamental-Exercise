19b244217f2207bfbe354e26c498ca108c50c864
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-01-19 22:51:23 +0600

    GenericStack

---
+++ src/Generic/GenericStack.java
@@ -0,0 +1,69 @@
+package Generic;
+
+public class GenericStack<T> {
+
+    private class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node top;
+    private int size;
+
+    public GenericStack() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (top == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() {
+        if (top == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        GenericStack<Integer> stack = new GenericStack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element is: " + stack.peek());
+        System.out.println("Stack size is: " + stack.size());
+
+        System.out.println("Popped element is: " + stack.pop());
+        System.out.println("Stack size after pop is: " + stack.size());
+
+        System.out.println("Is stack empty? " + stack.isEmpty());
+    }
+}
