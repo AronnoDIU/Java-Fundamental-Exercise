4121737250e9d5a4c22e4b124a273630f920d433
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-02-02 20:07:35 +0600

    MatrixMultiplication

---
+++ src/ArraysProblems/MatrixMultiplication.java
@@ -0,0 +1,55 @@
+package ArraysProblems;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrixA = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] matrixB = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+
+        int[][] result = multiplyMatrices(matrixA, matrixB);
+
+        System.out.println("Result of matrix multiplication:");
+        printMatrix(result);
+    }
+
+    public static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) {
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+
+        int[][] result = new int[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// Expected Output:
+
+// Result of matrix multiplication:
+// 30 24 18
+// 84 69 54
+// 138 114 90
